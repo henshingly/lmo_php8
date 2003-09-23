d03c2c9c3d4645cd03270ac550f4bd656f22cd69
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2003-09-23 16:13:37 +0000

    Mit Änderungen bitte bis zur Veröffentlichung der Pre-Alpha warten.

--- lmo-komplett/init.php
+++ lmo-komplett/init.php
@@ -1,10 +1,16 @@
 <?
+// Dies ist der Dateipfad zum LMO-Verzeichnis ausgehend von deinem Rootverzeichnis (ohne abschliessenden '/'!)
+$lmo_dateipfad="/tmp/lmo-komplett/lmo";
+// Dies ist die absolute URL zum LMO-Verzeichnis (ohne abschliessenden '/'!)
+$lmo_url='http://localhost/tmp/lmo-komplett/lmo';
+
+
 if (session_id()=="") session_start();
 @ini_set("session.use_trans_sid","1");
-if (!defined('PATH_TO_LMO'))        define('PATH_TO_LMO',         $_SERVER['DOCUMENT_ROOT'].'/tmp/lmo-komplett/lmo');
+if (!defined('PATH_TO_LMO'))        define('PATH_TO_LMO',         $_SERVER['DOCUMENT_ROOT'].$lmo_dateipfad);
 if (!defined('PATH_TO_ADDONDIR'))   define('PATH_TO_ADDONDIR',    PATH_TO_LMO.'/addon');
 if (!defined('PATH_TO_TEMPLATEDIR'))define('PATH_TO_TEMPLATEDIR', PATH_TO_LMO.'/template');
-if (!defined('URL_TO_LMO'))         define('URL_TO_LMO',          'http://localhost/tmp/lmo-komplett/lmo');
+if (!defined('URL_TO_LMO'))         define('URL_TO_LMO',          $lmo_url);
 if (!defined('URL_TO_TEMPLATEDIR')) define('URL_TO_TEMPLATEDIR',  URL_TO_LMO.'/template');
 if (!defined('URL_TO_ADDONDIR'))    define('URL_TO_ADDONDIR',     URL_TO_LMO.'/addon');
 if (!defined('URL_TO_IMGDIR'))      define('URL_TO_IMGDIR',       URL_TO_LMO.'/img');
