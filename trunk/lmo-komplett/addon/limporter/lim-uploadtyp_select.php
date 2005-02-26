1a15669a684db4e9d20099c01f9c35524367d290
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2005-02-26 18:00:58 +0000

    Limporter aus CVS entfernt

--- trunk/lmo-komplett/addon/limporter/lim-uploadtyp_select.php
+++
@@ -1,7 +0,0 @@
-  <tr>
-    <td class="lmost5" width="20">&nbsp;</td>
-    <td align="right">
-<nobr><acronym title="<? echo $text[175] ?>"><? echo "Quelle"; ?></acronym></nobr></td>
-    <td>&nbsp</td>
-<acronym title="<? echo $text[175] ?>"><select class="lmoadminein" name="xlocation" onChange="dolmoedit()"><? echo "<option value=\"0\""; if($ximporttype==0){echo " selected";} echo ">"."Lokale Datei"."</option>"; echo "<option value=\"1\""; if($ximporttype==1){echo " selected";} echo ">"."Internetadresse (URL)"."</option>"; ?></select></acronym></td>
-  </tr>
