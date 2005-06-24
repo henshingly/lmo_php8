a1b8d29605b1b9723773ff8a42e519184deee4d3
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2005-06-24 19:48:07 +0000

    Classlib Update Mininext v2.1.0 LMO v 4.0.0

--- trunk/lmo-komplett/lmo-langload.php
+++ trunk/lmo-komplett/lmo-langload.php
@@ -23,20 +23,42 @@
 
 $languages=array(
     'Deutsch'=>array('de_DE@euro','de_DE', 'de', 'ge', 'german'),
-    'Cestina'=>array('cs'),
-    'Francais'=>array('fr'),
-    'English'=>array('en'),
-    'Nederlands'=>array('nl'),
-    'Slovenskega'=>array('sl'),
-    'Magyar'=>array('hu'),
-    'Espanol'=>array('es'),
-    'Norsk'=>array('no'));
+    'Cestina'=>array('cs_CS'),
+    'Francais'=>array('fr_FR'),
+    'English'=>array('en_EN','en_US'),
+    'Nederlands'=>array('nl_NL'),
+    'Slovenskega'=>array('sl_SL'),
+    'Magyar'=>array('hu_HU'),
+    'Espanol'=>array('es_ES'),
+    'Norsk'=>array('no_NO'),
+    'Italiano'=>array('it_IT'),
+    'Polski'=>array('pl_PL'),
+    'Türkce'=>array('tr_TR')
+    );
 
 $text=array();
 
-read_langfile($text,PATH_TO_LANGDIR."/lang-Deutsch.txt"); //Temporär, bis Übersetzungen fertig sind
+if (!function_exists("read_langfile")) {
+  function read_langfile(&$text,$langfile,$addon="") {
+    if (($datei=@file($langfile))!==FALSE) {
+      foreach ($datei as $value) {
+        $paar=explode('=',trim($value),2);
+        if (isset($paar[0]) && is_numeric($paar[0])) {
+          if (!isset($paar[1])) $paar[1]="";
+          if (func_num_args()==2) {
+            $text[intval($paar[0])]=$paar[1];
+          }else{
+            $text[$addon][intval($paar[0])]=$paar[1];
+          }
+        }
+      }
+    }
+  }
+}
 
+read_langfile($text,PATH_TO_LANGDIR."/lang-Deutsch.txt");
 read_langfile($text,PATH_TO_LANGDIR."/lang-{$deflang}.txt");
+
 @setlocale (LC_TIME, $languages[$deflang][0]);  // PHP <4.3
 @setlocale (LC_TIME, $languages[$deflang]);     //PHP >4.3
 if (isset($lmouserlang) && $lmouserlang!=$deflang) {
@@ -45,7 +67,7 @@ if (isset($lmouserlang) && $lmouserlang!=$deflang) {
   setlocale (LC_TIME, $languages[$lmouserlang]);     //PHP >4.3
 }
 
-setlocale (LC_NUMERIC, 'en');  // Wichtig: Für Arithmetik immer englische Trennzeichen
+setlocale (LC_NUMERIC, 'en_EN');  // Wichtig: Für Arithmetik immer englische Trennzeichen
 
 
 //Alle lang-Dateien im Addon-Verzeichnis 
@@ -59,21 +81,6 @@ while (false!==($f=readdir($handle))) {
   }
 }
 
-function read_langfile(&$text,$langfile,$addon="") {
-  if (($datei=@file($langfile))!==FALSE) {
-    foreach ($datei as $value) {
-      $paar=explode('=',trim($value),2);
-      if (isset($paar[0]) && is_numeric($paar[0])) {
-        if (!isset($paar[1])) $paar[1]="";
-        if (func_num_args()==2) {
-          $text[intval($paar[0])]=$paar[1];
-        }else{
-          $text[$addon][intval($paar[0])]=$paar[1];
-        }
-	    }
-    }
-	}
-}
 $orgpkt=$text[37];
 $orgtor=$text[38];
 
@@ -99,5 +106,5 @@ $orgtor=$text[38];
 
 
 ///*
-  if (!function_exists("c")){function c($c){if($c==1)return(base64_decode("PGFjcm9ueW0gdGl0bGU9IkxpZ2EgTWFuYWdlciBPbmxpbmUiPkxNTzwvYWNyb255bT4mbmJzcDs0LjBSQzEmbmJzcDstJm5ic3A7PGEgaHJlZj0i"));return(base64_decode("aW5mbyIgdGl0bGU9IkluZm9ybWF0aW9uZW4genUgZGllc2VtIFBIUC1TY3JpcHQgdW5kIHNlaW5lbiBBdXRvcmVuIj6pJm5ic3A7MTk5Ny0yMDA1Jm5ic3A7TE1PLUdyb3VwPC9hPg=="));}}
-  if (!function_exists("d")){function d($c){if(strpos(htmlentities($c),htmlentities(base64_decode("PCEtLUluZm9saW5rLS0+")))>0){false;}else{ eval(base64_decode("ZWNobyAnPGFjcm9ueW0gdGl0bGU9IkxpZ2EgTWFuYWdlciBPbmxpbmUiPkxNTzwvYWNyb255bT4mbmJzcDs0LjBSQzEmbmJzcDstJm5ic3A7PGEgaHJlZj0iaHR0cDovL2xtby5zb3VyY2Vmb3JnZS5uZXQvaW5mbyIgdGl0bGU9IkluZm9ybWF0aW9uZW4genUgZGllc2VtIFBIUC1TY3JpcHQgdW5kIHNlaW5lbiBBdXRvcmVuIj6pJm5ic3A7MTk5Ny0yMDA1Jm5ic3A7TE1PLUdyb3VwPC9hPic7"));}}}//*/?>
+  if (!function_exists("c")){function c($c){if($c==1)return(base64_decode("PGFjcm9ueW0gdGl0bGU9IkxpZ2EgTWFuYWdlciBPbmxpbmUiPkxNTzwvYWNyb255bT4mbmJzcDs0LjAmbmJzcDstJm5ic3A7PGEgaHJlZj0i"));return(base64_decode("aHR0cDovL3d3dy5saWdhLW1hbmFnZXItb25saW5lLmRlLyIgdGl0bGU9IkNsaWNrIGhlcmUgdG8gZ2V0IGluZm9ybWF0aW9ucyBhYm91dCB0aGlzIHNjcmlwdCI+qSZuYnNwOzE5OTctMjAwNSZuYnNwO0xNTy1Hcm91cDwvYT4="));}}
+  if (!function_exists("d")){function d($c){if(strpos(htmlentities($c),htmlentities(base64_decode("PCEtLUluZm9saW5rLS0+")))>0){false;}else{ eval(base64_decode("ZWNobyAnPGFjcm9ueW0gdGl0bGU9IkxpZ2EgTWFuYWdlciBPbmxpbmUiPkxNTzwvYWNyb255bT4mbmJzcDs0LjAmbmJzcDstJm5ic3A7PGEgaHJlZj0iaHR0cDovL3d3dy5saWdhLW1hbmFnZXItb25saW5lLmRlLyIgdGl0bGU9IkNsaWNrIGhlcmUgdG8gZ2V0IGluZm9ybWF0aW9ucyBhYm91dCB0aGlzIHNjcmlwdCI+qSZuYnNwOzE5OTctMjAwNSZuYnNwO0xNTy1Hcm91cDwvYT4nOw=="));}}}//*/?>
