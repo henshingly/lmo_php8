76931fc6a109f485fba1a7b18954ee2223d67713
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2005-07-27 10:10:40 +0000

    http://www.liga-manager-online.de/phpBB2/viewtopic.php?p=28708 ff.

--- lmo-komplett/addon/mini/lmo-mininext.php
+++ lmo-komplett/addon/mini/lmo-mininext.php
@@ -287,7 +287,7 @@ if ($file && $liga->loadFile(PATH_TO_LMO.'/'.$dirliga.$file) == TRUE) {
 
     // Archivfolder lesen
 
-    if ($mininext_withArchiv==1 && readLigaDir(PATH_TO_LMO.'/'.$dirliga.$archivFolder,&$dataArray) == FALSE ) {
+    if ($mininext_withArchiv==1 && readLigaDir(PATH_TO_LMO.'/'.$dirliga.$archivFolder,$dataArray) == FALSE ) {
       echo getMessage($text['mini'][6]." ".PATH_TO_LMO.'/'.$dirliga.$archivFolder,TRUE);
     }
 
