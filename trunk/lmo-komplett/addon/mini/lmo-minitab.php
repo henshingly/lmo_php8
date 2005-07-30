9c17b93432051b14c768ca9d917165f98ecdec72
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2005-07-30 14:35:23 +0000

    Inkompatibilität mit alten output-Dateien beseitigt http://www.liga-manager-online.de/phpBB2/viewtopic.php?p=28663

--- trunk/lmo-komplett/addon/mini/lmo-minitab.php
+++ trunk/lmo-komplett/addon/mini/lmo-minitab.php
@@ -49,9 +49,9 @@ if (basename($_SERVER['PHP_SELF'])=="lmo-minitab.php") {?>
 }
 
 /**Format of CSV-File:
-  *       0     |      1      |         2         |   3   |  4    |  5   |  6   | 7   | 8 |  9 | 10 |  11
-  * TeamLongName|TeamShortName|TeamnameAbbrevation|Points+|Points-|Goals+|Goals-|Games|Win|Draw|Loss|Marking
-  *  Teamname   | Mittelname  |  Kurzname         |Pkt.+  | Pkt.- |Tore+ | Tore-|Sp.  | + | o  | -  |Markierung
+  *       0     |      1            |   2   |   3   |  4   |  5   |  6  | 7 | 8  |  9 |    10    |     11
+  * TeamLongName|TeamnameAbbrevation|Points+|Points-|Goals+|Goals-|Games|Win|Draw|Loss|Marking   |TeamShortName
+  *  Teamname   |  Kurzname         |Pkt.+  | Pkt.- |Tore+ | Tore-|Sp.  | + | o  | -  |Markierung| Mittelname  
   */
 if (file_exists(PATH_TO_LMO.'/'.$diroutput.$m_liga.'-tab.csv')) {
   $template = new HTML_Template_IT( PATH_TO_TEMPLATEDIR.'/mini' );
@@ -68,7 +68,7 @@ if (file_exists(PATH_TO_LMO.'/'.$diroutput.$m_liga.'-tab.csv')) {
   
   for ($i=0;$i<$m_anzteams;$i++) {
     if (empty($m_platz)) {
-      if (strpos($m_tabelle[$i][11],"F")!==FALSE) {
+      if (strpos($m_tabelle[$i][10],"F")!==FALSE) {
         break;
       }  
     } else {
@@ -98,50 +98,50 @@ if (file_exists(PATH_TO_LMO.'/'.$diroutput.$m_liga.'-tab.csv')) {
     $template->setVariable(array("Platz"=>"<strong>".($j+1)."</strong>"));
     $template->setVariable(array("TeamBild"=>getSmallImage($m_tabelle[$j][0])));
     $template->setVariable(array("TeamLang"=>$m_tabelle[$j][0]));
-    $template->setVariable(array("TeamMittel"=>$m_tabelle[$j][1]));
-    $template->setVariable(array("Team"=>$m_tabelle[$j][2]));
-    if ($m_tabelle[$j][4]=='') {
-      $template->setVariable(array("Punkte"=>$m_tabelle[$j][3]));
+    $template->setVariable(array("TeamMittel"=>(isset($m_tabelle[$j][11])?$m_tabelle[$j][11]:'')));
+    $template->setVariable(array("Team"=>$m_tabelle[$j][1]));
+    if ($m_tabelle[$j][3]=='') {
+      $template->setVariable(array("Punkte"=>$m_tabelle[$j][2]));
     } else {
-      $template->setVariable(array("Punkte"=>$m_tabelle[$j][3].':'.$m_tabelle[$j][4]));
+      $template->setVariable(array("Punkte"=>$m_tabelle[$j][2].':'.$m_tabelle[$j][3]));
     }
-    $template->setVariable(array("PlusTore"=>$m_tabelle[$j][5]));
-    $template->setVariable(array("MinusTore"=>$m_tabelle[$j][6]));
-    if (($m_diff=$m_tabelle[$j][5]-$m_tabelle[$j][6])>0) $m_diff='+'.$m_diff;
+    $template->setVariable(array("PlusTore"=>$m_tabelle[$j][4]));
+    $template->setVariable(array("MinusTore"=>$m_tabelle[$j][5]));
+    if (($m_diff=$m_tabelle[$j][4]-$m_tabelle[$j][5])>0) $m_diff='+'.$m_diff;
     $template->setVariable(array("Tordifferenz"=>$m_diff));
-    $template->setVariable(array("Spiele"=>$m_tabelle[$j][7]));
-    $template->setVariable(array("Siege"=>$m_tabelle[$j][8]));
-    $template->setVariable(array("Unentschieden"=>$m_tabelle[$j][9]));
-    $template->setVariable(array("Niederlagen"=>$m_tabelle[$j][10]));
+    $template->setVariable(array("Spiele"=>$m_tabelle[$j][6]));
+    $template->setVariable(array("Siege"=>$m_tabelle[$j][7]));
+    $template->setVariable(array("Unentschieden"=>$m_tabelle[$j][8]));
+    $template->setVariable(array("Niederlagen"=>$m_tabelle[$j][9]));
     $style='';
-    if ($m_tabelle[$j][11]!='') {
-      if (strpos($m_tabelle[$j][11],'M')!==FALSE){  //Meister
+    if ($m_tabelle[$j][10]!='') {
+      if (strpos($m_tabelle[$j][10],'M')!==FALSE){  //Meister
         $style="background: $lmo_tabelle_background1 repeat;";
         $style.=empty($lmo_tabelle_color1)?'':"color: $lmo_tabelle_color1;";
         $template->setVariable(array("Style"=>$style));
-      } elseif (strpos($m_tabelle[$j][11],'C')!==FALSE){  //CL
+      } elseif (strpos($m_tabelle[$j][10],'C')!==FALSE){  //CL
         $style="background: $lmo_tabelle_background2 repeat;";
         $style.=empty($lmo_tabelle_color2)?'':"color: $lmo_tabelle_color2;";
         $template->setVariable(array("Style"=>$style));
-      } elseif (strpos($m_tabelle[$j][11],'Q')!==FALSE){  //CL-Quali
+      } elseif (strpos($m_tabelle[$j][10],'Q')!==FALSE){  //CL-Quali
         $style="background: $lmo_tabelle_background3 repeat;";
         $style.=empty($lmo_tabelle_color3)?'':"color: $lmo_tabelle_color3;";
         $template->setVariable(array("Style"=>$style));
-      } elseif (strpos($m_tabelle[$j][11],'U')!==FALSE){   //UEFA
+      } elseif (strpos($m_tabelle[$j][10],'U')!==FALSE){   //UEFA
         $style="background: $lmo_tabelle_background4 repeat;";
         $style.=empty($lmo_tabelle_color4)?'':"color: $lmo_tabelle_color4;";
         $template->setVariable(array("Style"=>$style));
-      } elseif (strpos($m_tabelle[$j][11],'R')!==FALSE){  //Relegation
+      } elseif (strpos($m_tabelle[$j][10],'R')!==FALSE){  //Relegation
         $style="background: $lmo_tabelle_background5 repeat;";
         $style.=empty($lmo_tabelle_color5)?'':"color: $lmo_tabelle_color5;";
         $template->setVariable(array("Style"=>$style));
-      } elseif (strpos($m_tabelle[$j][11],'A')!==FALSE){  //Absteiger
+      } elseif (strpos($m_tabelle[$j][10],'A')!==FALSE){  //Absteiger
         $style="background: $lmo_tabelle_background6 repeat;";
         $style.=empty($lmo_tabelle_color6)?'':"color: $lmo_tabelle_color6;";
         $template->setVariable(array("Style"=>$style));
       }
     }
-    if (strpos($m_tabelle[$j][11],'F')!==FALSE){  //FavTeam
+    if (strpos($m_tabelle[$j][10],'F')!==FALSE){  //FavTeam
       $style.="font-weight:bolder;";
       $template->setVariable(array("Style"=>$style));
     }
