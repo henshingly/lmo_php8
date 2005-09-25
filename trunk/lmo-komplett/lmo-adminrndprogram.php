dfaeb801dfb0556c68f73eeda2a4fed018124418
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2005-09-25 11:15:21 +0000

    neuer Algorithmus für zufälligen Spielplan von sebman

--- trunk/lmo-komplett/lmo-adminrndprogram.php
+++ trunk/lmo-komplett/lmo-adminrndprogram.php
@@ -7,7 +7,7 @@
   * modify it under the terms of the GNU General Public License as
   * published by the Free Software Foundation; either version 2 of
   * the License, or (at your option) any later version.
-  * 
+  *
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -17,81 +17,136 @@
   *
   */
   
-  
+  /**
+   * @version 1.0 2005-09-22
+   * @author Sebastian Janke, s.janke@tu-bs.de
+   * @return Die fertige Kreuztabelle.
+   *
+   * Schreibt die Kreuztabelle der Begegnungen (Format s.u.).<br>
+   * Das Vergabesystem arbeitet mit einem Tischemodell.<br>
+   * An jedem Tisch sitzen sich 2 Spieler gegen&uuml;ber.<br>
+   * Jeder Tisch hat eine Heimrecht- und eine Ausw&auml;rtsseite.<br>
+   * <u>Beispiel mit 4 Tischen und 8 Sitzpl&auml;tzen:</u><br>
+   * Die Pl&auml;tze werden in der Reihenfolge 0-7 vergeben.<br>
+   * 7 1 2 3<br>
+   * X o-o-o\<br>
+   * T/T l T|<br>
+   * o-o-o-o/<br>
+   * 0 6 5 4<br>
+   * <u>Legende:</u><br>
+   * T = Tisch (Heimrecht oben)<br>
+   * l = Tisch (Heimrecht unten)<br>
+   * X = Spieler mit dem festen Sitzplatz<br>
+   * o = Spieler, der gegen den Uhrzeigersinn rotieren muss.<br>
+   * - | / \ = Rotationswege der beweglichen Spieler.<br>
+   * <br>
+   * Zu Beginn eines neues Spieltages rutschen alle Spieler ohne festen Sitzplatz (0-6) jeweils einen Platz gegen den
+   * Uhrzeigersin weiter.<br>
+   * <br>
+   * Die Positionsnummer von X ist immer der h&ouml;chste auftretende Index.<br>
+   * <u>Format der Kreuztabelle:</u><br>
+   * Der erste Index des Arrays (Zeilen) greift auf den Spieltag zu, der zweite
+   * Index bestimmt den Spieler mit Heimrecht. Der Wert an der Stelle des Arrays entspricht der
+   * Spaltennummer (=Spielernummer) des gegnerischen Spielers, dieser spielt ausw&auml;rts. Wenn
+   * kein Spiel eingetragen ist, ist der Wert -1 (Array-Initilialisierung mit -1).<br>
+   */
+
+
+
 require_once(PATH_TO_LMO."/lmo-admintest.php");
 
-$plan = array_pad($array, 41, "");
-for($i = 1; $i < 41; $i++) {
-  $plan[$i] = array_pad($array, 41, "");
-  for($j = 1; $j < 41; $j++) {
-    if ($i == $j) {
-      $plan[$i][$j] = "-1";
-    } else {
-      $plan[$i][$j] = "0";
-    }
+/** &Auml;nderungen:
+ * a) 1. Index verdoppelt, entspricht jetzt den Spieltagen statt Spieler-Nummer.
+ * b) Indizes beginnen bei 0 statt 1.
+ * c) Kreuztabellen-Initialiserung komplett mit '-1' statt Hauptdiagonale mit '0'
+ */
+$plan = array_pad($array, 80, "");
+for($i = 0; $i < 40; $i++) {
+  $plan[$i] = array_pad($array, 40, "");
+  for($j = 0; $j < 40; $j++) {
+    $plan[$i][$j] = "-1";
   }
 }
 $j = $xteams;
 if (($xteams%2) == 1) {
   $j++;
 }
-for($a = 1; $a <= $j; $a++) {
-  $b = $a;
-  if ($a == 1) {
-    $b--;
+
+// Selbsterkl&auml;rend
+$spieltageHinrunde = $j - 1;
+$spieltage = ($j - 1) * 2;
+
+// Hat der feste Platz zu Beginn Heimrecht? Dieses Recht wechselt an jedem Spieltag, der Tisch wird gedreht.
+$festerPlatzIsHome = true;
+
+// Spieler X, Sitzbelegung durch Spieler aendert sich nicht
+$spielerNrFesterPlatz = $j - 1;
+
+// Tischpartner von Spieler X
+$spielerNrGegenueberFesterPlatz = 0;
+
+/* Hauptschleife, l&auml;uft sequentiell &uuml;ber die Spieltage der Hinrunde.
+ * Die R&uuml;ckrunde wird sofort gespiegelt mit geschrieben.
+ */
+for ($spieltag = 0; $spieltag < $spieltageHinrunde; $spieltag++) {
+
+  /**
+   * 1. Abschnitt: Paarung des Spielers auf dem festen Sitzplatz
+   */
+  $spielerNrGegenueberFesterPlatz = $spieltag;
+  if (festerPlatzIsHome) {
+    // Hinrunde
+    $plan[$spieltag][$spielerNrFesterPlatz] = $spielerNrGegenueberFesterPlatz;
+    // Rueckrunde
+    $plan[$spieltag+$spieltageHinrunde][$spielerNrGegenueberFesterPlatz] = $spielerNrFesterPlatz;
+  } 
+  else {
+    // Hinrunde
+    $plan[$spieltag][$spielerNrGegenueberFesterPlatz] = $spielerNrFesterPlatz;
+    // R&uuml;ckrunde
+    $plan[$spieltag+$spieltageHinrunde][$spielerNrFesterPlatz] = $spielerNrGegenueberFesterPlatz;
   }
-  for($i = 1; $i <= $j; $i++) {
-    if ($plan[$i][$a] != "-1") {
-      $plan[$i][$a] = $b;
-      $b++;
-      if ($b >= $j) {
-        $b = 1;
-      }
-    } else {
-      if ($i > 1) {
-        $b++;
-      }
-    }
+  
+  /**
+   * 2. Abschnitt: Alle weiteren Paarungen
+   */
+  $beweglicheSpieler = $j - 1;
+  $zielSitzGerade = 2;
+  $zielSitzUngerade = $beweglicheSpieler - $zielSitzGerade;
+  $zielSitzGeradeVerschoben = ($zielSitzGerade + $spieltag) % $beweglicheSpieler;
+  $zielSitzUngeradeVerschoben = ($zielSitzUngerade + $spieltag) % $beweglicheSpieler;
+  
+  /**
+   * Innere Schleife, l&auml;uft &uuml;ber alle Paarungen EINES Spieltages.
+   */
+  while ($zielSitzGerade < $beweglicheSpieler) {
+    // Hinrunde
+    $plan[$spieltag][$zielSitzGeradeVerschoben] = $zielSitzUngeradeVerschoben;
+    // R&uuml;ckrunde
+    $plan[$spieltag+$spieltageHinrunde][$zielSitzUngeradeVerschoben] = $zielSitzGeradeVerschoben;
+    
+    // Weiterrutschen der beweglichen Spieler
+    $zielSitzGerade += 2;
+    $zielSitzUngerade -= 2;
+    $zielSitzGeradeVerschoben = ($zielSitzGerade + $spieltag) % $beweglicheSpieler;
+    $zielSitzUngeradeVerschoben = ($zielSitzUngerade + $spieltag) % $beweglicheSpieler;
   }
-}
-$a = 2;
-$b = 1;
-for($i = 2; $i < $j; $i++) {
-  if (($plan[$a][$b]+1) < $j) {
-    $plan[$i][$j] = $plan[$a][$b]+1;
-  } else {
-    $plan[$i][$j] = 1;
-  }
-  $plan[$j][$i] = $plan[$i][$j];
-  $a++;
-  $b++;
-}
-for($a = 1; $a < $j; $a++) {
-  for($b = $a+1; $b <= $j; $b++) {
-    if ((($a+$b)%2) == 0) {
-      $plan[$a][$b] = $plan[$b][$a]+$j-1;
-    } else {
-      $plan[$b][$a] = $plan[$a][$b]+$j-1;
-    }
-  }
-}
-for($a = 1; $a <= $xteams; $a++) {
-  for($b = 1; $b <= $xteams; $b++) {
-    if ($plan[$a][$b] > 0) {
-      $l = 0;
-      for($i = 1; $i <= $xanzsp; $i++) {
-        if (!isset($yteama[$plan[$a][$b]-1][$i-1])) {
-          $yteama[$plan[$a][$b]-1][$i-1] = 0;
-        }
-        if (($yteama[$plan[$a][$b]-1][$i-1] == 0) && ($l == 0)) {
-          $yteama[$plan[$a][$b]-1][$i-1] = $a;
-          $yteamb[$plan[$a][$b]-1][$i-1] = $b;
-          $l = 1;
-        }
-      }
-    }
-  }
+  // 1. Tisch (mit festem Spieler) drehen, Heimrecht wechselt
+  $festerPlatzIsHome = !$festerPlatzIsHome;
 }
 
-
+/**
+ * Schleife, die die Textfelder des LMO-Spielplans schreibt.
+ */
+for($spieltag = 0; $spieltag < $spieltage; $spieltag++) {
+  $spielnr = 0;
+  for($heim = 0; $heim < $j; $heim++) {
+    // Anpassung der Spielernummer noetig, meine beginnen bei 0, LMO-Nummern bei 1. Deswegen +1!
+    if ($plan[$spieltag][$heim] > -1) {
+      $yteama[$spieltag][$spielnr] = $heim+1;
+      $yteamb[$spieltag][$spielnr] = $plan[$spieltag][$heim]+1;
+      $spielnr++;
+    }
+  }
+}
 ?>
