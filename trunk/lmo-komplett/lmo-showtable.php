6b15e40880100abf942666e1ac8843cb539d0a02
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2007-02-25 09:59:32 +0000

    http://www.liga-manager-online.de/lmo-forum/viewtopic.php?t=7969

--- trunk/lmo-komplett/lmo-showtable.php
+++ trunk/lmo-komplett/lmo-showtable.php
@@ -7,7 +7,7 @@
   * modify it under the terms of the GNU General Public License as
   * published by the Free Software Foundation; either version 2 of
   * the License, or (at your option) any later version.
-  * 
+  *
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -56,67 +56,67 @@ if($tabonres==2){?>
     <th align="left" colspan="<?=$breite; ?>"><?=$text[41]; ?></th>
     <th width="2">&nbsp;</th>
     <th align="left" colspan="<?=$breite; ?>"><?=$text[42]; ?></th>
-  </tr><? 
+  </tr><?
 }?>
   <tr>
     <th align="left" colspan="7"><?if($tabonres!=2){echo $tabdat;}?>&nbsp;</th>
     <th align="right"><?=$text[33]; ?></th>
-    <th align="right"><?=$text[34]; ?></th><?  
+    <th align="right"><?=$text[34]; ?></th><?
 if($hidr!=1){?>
     <th align="right"><?=$text[35]; ?></th><?
 }?>
-    <th align="right"><?=$text[36]; ?></th><?  
+    <th align="right"><?=$text[36]; ?></th><?
 if($tabpkt==0){?>
     <th width="2">&nbsp;</th>
-    <th <?=$dummy?>><?=$text[37]?></th><? 
+    <th <?=$dummy?>><?=$text[37]?></th><?
 }?>
     <th width="2">&nbsp;</th>
     <th colspan="3" align="center"><?=$text[38]; ?></th>
     <th align="right"><?=$text[39]; ?></th><?
 if($tabpkt==1){?>
     <th width="2">&nbsp;</th>
-    <th <?=$dummy?>><?=$text[37]?></th><? 
+    <th <?=$dummy?>><?=$text[37]?></th><?
 }
 if($tabonres==2){?>
     <th width="2">&nbsp;</th>
     <th align="right"><?=$text[33]; ?></th>
     <th align="right"><?=$text[34]; ?></th><?
   if($hidr!=1){?>
-    <th align="right"><?=$text[35]?></th><? 
+    <th align="right"><?=$text[35]?></th><?
   } ?>
-    <th align="right"><?=$text[36]; ?></th><?  
+    <th align="right"><?=$text[36]; ?></th><?
   if($tabpkt==0){?>
     <th width="2">&nbsp;</th>
-    <th <?=$dummy?>><?=$text[37]?></th><? 
+    <th <?=$dummy?>><?=$text[37]?></th><?
   } ?>
     <th width="2">&nbsp;</th>
     <th colspan="3" align="center"><?=$text[38]; ?></th>
-    <th align="right"><?=$text[39]; ?></th><?  
+    <th align="right"><?=$text[39]; ?></th><?
   if($tabpkt==1){?>
     <th width="2">&nbsp;</th>
-    <th <?=$dummy?>><?=$text[37]?></th><? 
+    <th <?=$dummy?>><?=$text[37]?></th><?
   }?>
     <th width="2">&nbsp;</th>
     <th align="right"><?=$text[33]; ?></th>
-    <th align="right"><?=$text[34]; ?></th><?  
+    <th align="right"><?=$text[34]; ?></th><?
   if($hidr!=1){?>
-    <th align="right"><?=$text[35]?></th><? 
+    <th align="right"><?=$text[35]?></th><?
   }?>
-    <th align="right"><?=$text[36]; ?></th><?  
+    <th align="right"><?=$text[36]; ?></th><?
   if($tabpkt==0){?>
     <th width="2">&nbsp;</th>
-    <th<?=$dummy?>><?=$text[37]?></th><? 
+    <th<?=$dummy?>><?=$text[37]?></th><?
   }?>
     <th width="2">&nbsp;</th>
     <th colspan="3" align="center"><?=$text[38]; ?></th>
-    <th align="right"><?=$text[39]; ?></th><?  
+    <th align="right"><?=$text[39]; ?></th><?
   if($tabpkt==1){?>
     <th width="2">&nbsp;</th>
-    <th <?=$dummy?>><?=$text[37]?></th><? 
+    <th <?=$dummy?>><?=$text[37]?></th><?
   }
 }?>
   </tr><?
-   
+
 $j = 1;
 for($x = 1; $x <= $anzteams; $x++) {
   $i = intval(substr($tab0[$x-1], 34));
@@ -148,10 +148,10 @@ for($x = 1; $x <= $anzteams; $x++) {
     if (($x <= $anzteams) && ($x > $anzteams-$anzab) && ($anzab > 0)) {
       $lmo_tabelle_class = "lmoTabelleAbsteiger nobr";
     }
-    
+
   }?>
   <tr>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$x.$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$x.$dumm2; ?></td><?
   $y = 0;
   if ($endtab > 1 && isset($platz0[$i])) {
     if ($platz0[$i] < $platz1[$i]) {
@@ -162,25 +162,25 @@ for($x = 1; $x <= $anzteams; $x++) {
   }?>
     <td class="<?=$lmo_tabelle_class?>"><img src='<?=URL_TO_IMGDIR."/lmo-tab".$y.".gif";?>' width="9" height="9" border="0" alt=''></td>
     <td class="<?=$lmo_tabelle_class?>" align="center"><?=getSmallImage($teams[$i]);?></td>
-    <td class="<?=$lmo_tabelle_class; ?>" align="left"><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="left"><?
   echo $dummy.$teams[$i].$dumm2;
   if (($teamu[$i] != "") && ($urlt == 1)) {?>
         <a href="<?=$teamu[$i]?>" target="_blank"><img border="0" title="<?=$text[46]?>" width="11" src="<?=URL_TO_IMGDIR."/url.png";?>" alt="<?=$text[564]?>"></a><?
   }?>
-  
+
     </td>
     <td class="<?=$lmo_tabelle_class; ?>" width="2">&nbsp;</td>
-    <td class="<?=$lmo_tabelle_class; ?>"><? 
-    
+    <td class="<?=$lmo_tabelle_class; ?>"><?
+
   if ($teamn[$i]!="" || $strafp[$i]!=0 || $strafm[$i]!=0 || $torkorrektur1[$i]!=0 || $torkorrektur2[$i]!=0) {
     $lmo_tabellennotiz=getSmallImage($teams[$i]);
-    
+
     /** Notizen anzeigen
      *
      * Achtung: Da beim Speichern Strafpunkte/-tore positiv sind und Bonuspunkte negativ (altes System des LMO)
      * muss mit -1 multipliziert werden, um die Punkte/Tore richtig anzuzeigen
      */
-    
+
     $lmo_tabellennotiz.=" <strong>".$teams[$i]."</strong>";
     //Straf-/Bonuspunkte
     if ($strafp[$i]!=0 || $strafm[$i]!=0) {
@@ -189,7 +189,7 @@ for($x = 1; $x <= $anzteams; $x++) {
       $lmo_tabellennotiz.=$strafp[$i]<0?"+".((-1)*applyFactor($strafp[$i],$pointsfaktor)):((-1)*applyFactor($strafp[$i],$pointsfaktor));
       //Minuspunkte
       if ($minus==2) {
-        $lmo_tabellennotiz.=":".$strafm[$i]<0?"+".((-1)*applyFactor($strafm[$i],$pointsfaktor)):((-1)*applyFactor($strafm[$i],$pointsfaktor));
+        $lmo_tabellennotiz.=":".($strafm[$i]<0?"+".((-1)*applyFactor($strafm[$i],$pointsfaktor)):((-1)*applyFactor($strafm[$i],$pointsfaktor)));
       }
       //Ab ST
       if ($strafdat[$i]!=0) $lmo_tabellennotiz.=" ({$text[524]} {$text[145]} {$strafdat[$i]})";
@@ -218,9 +218,9 @@ for($x = 1; $x <= $anzteams; $x++) {
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$spiele[$i].$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$siege[$i].$dumm2; ?></td><?
   if($hidr!=1){ ?>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$unent[$i].$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$unent[$i].$dumm2; ?></td><?
   } ?>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$nieder[$i].$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$nieder[$i].$dumm2; ?></td><?
   if ($tabpkt == 0) {?>
     <td class="<?=$lmo_tabelle_class?>" width="2">&nbsp;</td>
     <td class="<?=$lmo_tabelle_class?>" align="right"><strong><?=applyFactor($punkte[$i],$pointsfaktor)?></strong></td><?
@@ -229,12 +229,12 @@ for($x = 1; $x <= $anzteams; $x++) {
     <td class="<?=$lmo_tabelle_class?>"><strong><?=applyFactor($negativ[$i],$pointsfaktor)?></strong></td><?
     }
   }?>
-      
+
     <td class="<?=$lmo_tabelle_class; ?>" width="2">&nbsp;</td>
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($etore[$i],$goalfaktor).$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="center" width="4"><?=$dummy; ?>:<?=$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>"><?=$dummy.applyFactor($atore[$i],$goalfaktor).$dumm2; ?></td>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($dtore[$i],$goalfaktor).$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($dtore[$i],$goalfaktor).$dumm2; ?></td><?
   if($tabpkt==1){?>
     <td class="<?=$lmo_tabelle_class?>" width="2">&nbsp;</td>
     <td class="<?=$lmo_tabelle_class?>" align="right"><strong><?=applyFactor($punkte[$i],$pointsfaktor)?></strong></td><?
@@ -249,7 +249,7 @@ for($x = 1; $x <= $anzteams; $x++) {
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$hspiele[$i].$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$hsiege[$i].$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$hunent[$i].$dumm2; ?></td>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$hnieder[$i].$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$hnieder[$i].$dumm2; ?></td><?
     if($tabpkt==0){?>
     <td class="<?=$lmo_tabelle_class?>" width="2">&nbsp;</td>
     <td class="<?=$lmo_tabelle_class?>" align="right"><strong><?=applyFactor($hpunkte[$i],$pointsfaktor)?></strong></td><?
@@ -262,7 +262,7 @@ for($x = 1; $x <= $anzteams; $x++) {
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($hetore[$i],$goalfaktor).$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="center" width="4"><?=$dummy; ?>:<?=$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>"><?=$dummy.applyFactor($hatore[$i],$goalfaktor).$dumm2; ?></td>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($hdtore[$i],$goalfaktor).$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($hdtore[$i],$goalfaktor).$dumm2; ?></td><?
     if($tabpkt==1){?>
     <td class="<?=$lmo_tabelle_class?>" width="2">&nbsp;</td>
     <td class="<?=$lmo_tabelle_class?>" align="right"><strong><?=applyFactor($hpunkte[$i],$pointsfaktor)?></strong></td><?
@@ -276,7 +276,7 @@ for($x = 1; $x <= $anzteams; $x++) {
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$aspiele[$i].$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$asiege[$i].$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$aunent[$i].$dumm2; ?></td>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$anieder[$i].$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.$anieder[$i].$dumm2; ?></td><?
     if($tabpkt==0){?>
     <td class="<?=$lmo_tabelle_class?>" width="2">&nbsp;</td>
     <td class="<?=$lmo_tabelle_class?>" align="right"><strong><?=applyFactor($apunkte[$i],$pointsfaktor)?></strong></td><?
@@ -289,7 +289,7 @@ for($x = 1; $x <= $anzteams; $x++) {
     <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($aetore[$i],$goalfaktor).$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>" align="center" width="4"><?=$dummy; ?>:<?=$dumm2; ?></td>
     <td class="<?=$lmo_tabelle_class; ?>"><?=$dummy.applyFactor($aatore[$i],$goalfaktor).$dumm2; ?></td>
-    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($adtore[$i],$goalfaktor).$dumm2; ?></td><? 
+    <td class="<?=$lmo_tabelle_class; ?>" align="right"><?=$dummy.applyFactor($adtore[$i],$goalfaktor).$dumm2; ?></td><?
     if($tabpkt==1){?>
     <td class="<?=$lmo_tabelle_class?>" width="2">&nbsp;</td>
     <td class="<?=$lmo_tabelle_class?>" align="right"><strong><?=applyFactor($apunkte[$i],$pointsfaktor)?></strong></td><?
@@ -301,8 +301,8 @@ for($x = 1; $x <= $anzteams; $x++) {
   }  /*tabonres==2*/?>
   </tr><?
 }/*for*/?>
-  <tr>  
-    <td class="lmoFooter" align="center" colspan="<?=$gesamtbreite?>">&nbsp;<?  
+  <tr>
+    <td class="lmoFooter" align="center" colspan="<?=$gesamtbreite?>">&nbsp;<?
 if ($einzutoretab == 1) {
   $zustat_file = str_replace(".l98", ".l98.php",  basename($file));
   $zustat_dir = basename($diroutput);
