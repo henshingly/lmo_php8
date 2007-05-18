3e60546158c9f901c7d980d3963f48ec95821c20
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2007-05-18 09:22:39 +0000

    entfernt

--- trunk/limporter/lmo_root/addon/limporter/lim-filecontent.php
+++
@@ -1,9 +0,0 @@
-  <tr>
-    <td class="lmost4" colspan="3"><nobr>&nbsp;<?PHP echo $text['limporter'][91];?></nobr></td>
-  </tr>
-  <tr>
-    <td class="lmost5" colspan="3"><nobr>&nbsp;
-    <textarea name="fileContent" class="lmoadminein" cols="60" rows="10" wrap="OFF"><?PHP echo $liga->fileContent(); ?></textarea>
-    <br>&nbsp;</nobr>
-    </td>
-  </tr>
