f9e3d7806f57e06f84e028aa205460c12292195a
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2009-10-03 14:56:53 +0000

    removed old function

--- lmo/addon/viewer/viewer_functions.php
+++ lmo/addon/viewer/viewer_functions.php
@@ -7,7 +7,7 @@
   * modify it under the terms of the GNU General Public License as
   * published by the Free Software Foundation; either version 2 of
   * the License, or (at your option) any later version.
-  * 
+  *
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -17,24 +17,13 @@
   *
   * $Id$
   */
-  
-function getBigImage($team,$alternative_text='') {
-  $team=str_replace("/","",$team);
-  if (!file_exists(PATH_TO_IMGDIR."/teams/big/".$team.".gif")) {
-    $team=preg_replace("/[^a-zA-Z0-9]/",'',$team);
-  }
-  if (!file_exists(PATH_TO_IMGDIR."/teams/big/".$team.".gif")) {
-    return $alternative_text;
-  } else {
-    $imgdata=getimagesize(PATH_TO_IMGDIR."/teams/big/".$team.".gif");
-    return ("<img border='0' src='".URL_TO_IMGDIR."/teams/big/".rawurlencode($team).".gif' {$imgdata[3]} alt='$alternative_text'> ");
-  }
-}
+
+
 
 function zeitberechnung($modus,$wert)  {
-// Gibt verschiedene Werte je nach $Modus zur?ck:
-// 1 = Tag + $wert (Tage) um 0:00 Uhr als Zeitstempel
-// 2 = Tag + $wert (Tage) um 23:59 Uhr als Zeitstempel
+  // Gibt verschiedene Werte je nach $Modus zur?ck:
+  // 1 = Tag + $wert (Tage) um 0:00 Uhr als Zeitstempel
+  // 2 = Tag + $wert (Tage) um 23:59 Uhr als Zeitstempel
 
   switch($modus) {
     case "1":
@@ -42,13 +31,13 @@ function zeitberechnung($modus,$wert)  {
       // Gibt heutigen Tag + $wert (Tage) um 0:00 Uhr als Zeitstempel zurück
       break;
 
-      case "2":
-        return mktime(23,59,0,date("m"), date("d")+$wert,date("Y"));
-        // Gibt heutigen Tag + $wert (Tage) um 23:59 Uhr als Zeitstempel zurück
-        break;
+    case "2":
+      return mktime(23,59,0,date("m"), date("d")+$wert,date("Y"));
+      // Gibt heutigen Tag + $wert (Tage) um 23:59 Uhr als Zeitstempel zurück
+      break;
 
-      default:
-        return false;
+    default:
+      return false;
   }   // switch
 }
 
