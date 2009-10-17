0099bfd5cdbf7fa36a117bf0c15401a1304ed481
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2009-10-17 12:29:45 +0000

    white space & indentation fixes added some phpdocs

--- lmo/addon/classlib/html_output.php
+++ lmo/addon/classlib/html_output.php
@@ -1,25 +1,25 @@
 <?php
 /** Liga Manager Online 4
-  *
-  * http://lmo.sourceforge.net/
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU General Public License as
-  * published by the Free Software Foundation; either version 2 of
-  * the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  * General Public License for more details.
-  *
-  * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
-  *
-  *
-  * @package classLib
-  * @access   public
-  * @version $Id$
-  */
+ *
+ * http://lmo.sourceforge.net/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
+ *
+ *
+ * @package classLib
+ * @access   public
+ * @version $Id$
+ */
 
 
 /**
