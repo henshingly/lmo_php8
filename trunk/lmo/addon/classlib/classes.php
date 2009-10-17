7a4c819bae9c8a89e56b577bd7ee37a26624b9cf
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2009-10-17 12:29:45 +0000

    white space & indentation fixes added some phpdocs

--- trunk/lmo/addon/classlib/classes.php
+++ trunk/lmo/addon/classlib/classes.php
@@ -6,52 +6,34 @@
  * in Form eines Objektbaums ab und ermöglicht so die Entwicklung von
  * objektorientierten Erweiterung in Form von sog. Addons für den Liga Manager Online.
  *
- * <br>Änderungen:
- * <br> 20.07.04 2.0 Start
- * <br> 22.07.04 2.1 Pdf Class hinzugefügt.
- * <br> 23.07.04 2.1 Bugfix in methode $liga->writeFile teamdetails gingen verloren
- * <br> 31.07.04     relative Pfadangabe in class.ezpdf.php ersetzt
- * <br> 01.08.04 2.2 Spielbericht (reportURL) zur class Partie hinzugefügt
- * <br>              Beim Einlesen der Runden wird jetzt der Pokalmodus berücksichtigt
- * <br> 20.09.04 2.3 Class iniFile function getIniFile
- * <br>              Beim Auslesen einer URL, die keyValues enthält
- * <br>              fehlten die Gleichheitszeichen
- * <br> 22.09.04 2.3 Pokalmodus in loadLiga implementiert
- * <br> 22.11.04 2.4 Mal wieder keyValue gefunden und in keyValues geändert
- * <br>              classes.php Ist der aktuelle Spieltag nicht gesetzt wird $aktSpTag = 1
- * <br> 02.12.04 2.5 writeFile / loadFile geändert
- * <br>              Das komplette File wird zunächst in ein array geladen und zwischengespeichert.
- * <br>              Beim Speichern wird dieses zunächst mit den Werten der Objekte abgeglichen und
- * <br>              dann im Anschluss komplett ins file geschrieben. Dadurch gehen keine Informationen verloren
- * <br>              Auch Erweiterungen des LigaFiles (z.B, zusätzliche Sektionen wie beim MittellangTeamName
- * <br>              Addon werden erkannt und geschrieben.
- * <br> 20.12.04 2.6 neue Fktion gamesSorted hinzugefügt (Sortierung der Partien)
- * <br> 14.01.05 2.6 SP1 Klasse Partie function valuateGame() Ermittelt die Wertung einer Partie (beta Status)
- * <br> 16.02.05 2.7 Klasse Partie fkt. gToreString() und hToreString() überarbeitet. Bei greenTable wird 0 bzw. 0*
+ * Änderungen:
+ *  20.07.04 2.0 Start
+ *  22.07.04 2.1 Pdf Class hinzugefügt.
+ *  23.07.04 2.1 Bugfix in methode $liga->writeFile teamdetails gingen verloren
+ *  31.07.04     relative Pfadangabe in class.ezpdf.php ersetzt
+ *  01.08.04 2.2 Spielbericht (reportURL) zur class Partie hinzugefügt
+ *               Beim Einlesen der Runden wird jetzt der Pokalmodus berücksichtigt
+ *  20.09.04 2.3 Class iniFile function getIniFile
+ *               Beim Auslesen einer URL, die keyValues enthält
+ *               fehlten die Gleichheitszeichen
+ *  22.09.04 2.3 Pokalmodus in loadLiga implementiert
+ *  22.11.04 2.4 Mal wieder keyValue gefunden und in keyValues geändert
+ *               classes.php Ist der aktuelle Spieltag nicht gesetzt wird $aktSpTag = 1
+ *  02.12.04 2.5 writeFile / loadFile geändert
+ *               Das komplette File wird zunächst in ein array geladen und zwischengespeichert.
+ *               Beim Speichern wird dieses zunächst mit den Werten der Objekte abgeglichen und
+ *               dann im Anschluss komplett ins file geschrieben. Dadurch gehen keine Informationen verloren
+ *               Auch Erweiterungen des LigaFiles (z.B, zusätzliche Sektionen wie beim MittellangTeamName
+ *               Addon werden erkannt und geschrieben.
+ *  20.12.04 2.6 neue Fktion gamesSorted hinzugefügt (Sortierung der Partien)
+ *  14.01.05 2.6 SP1 Klasse Partie function valuateGame() Ermittelt die Wertung einer Partie (beta Status)
+ *  16.02.05 2.7 Klasse Partie fkt. gToreString() und hToreString() überarbeitet. Bei greenTable wird 0 bzw. 0*
  *									 zurückgegeben.
- * <br> 10.04.05 2.7 (Dank an Gowi) update Funktionen für addons implementiert
+ *  10.04.05 2.7 (Dank an Gowi) update Funktionen für addons implementiert
  * @author    Tim Schumacher <webobjects@gmx.net>
  * @version   2.7
  * @package   classLib
 */
-//
-// LMO Class Library Version (01/2004)
-// Copyright (C) 2004 by Tim Schumacher
-// webobjects@gmx.net /
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of
-// the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 //Team
 require(PATH_TO_ADDONDIR."/classlib/classes/team.class.php");
--- trunk/lmo/addon/classlib/ini.php
+++ trunk/lmo/addon/classlib/ini.php
@@ -5,52 +5,11 @@
  * Copyright (C) 2003 by Tim Schumacher
  * timme@uni.de /
  *
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- *
- * <br>Änderungen:
- * <br> 20.07.04 2.0 Start
- * <br> 22.07.04 2.1 Pdf Class hinzugefügt.
- * <br> 23.07.04 2.1 Bugfix in methode $liga->writeFile teamdetails gingen verloren
- * <br> 31.07.04     relative Pfadangabe in class.ezpdf.php ersetzt
- * <br> 01.08.04 2.2 Spielbericht (reportURL) zur class Partie hinzugefügt
- * <br>              Beim Einlesen der Runden wird jetzt der Pokalmodus berücksichtigt
- * <br> 20.09.04 2.3 Class iniFile function getIniFile
- * <br>              Beim Auslesen einer URL, die keyValues enthält
- * <br>              fehlten die Gleichheitszeichen
- * <br> 22.09.04 2.3 Pokalmodus in loadLiga implementiert
- * <br> 22.11.04 2.4 Mal wieder keyValue gefunden und in keyValues geändert
- * <br>              classes.php Ist der aktuelle Spieltag nicht gesetzt wird $aktSpTag = 1
- * <br> 02.12.04 2.5 writeFile / loadFile geändert
- * <br>              Das komplette File wird zunächst in ein array geladen und zwischengespeichert.
- * <br>              Beim Speichern wird dieses zunächst mit den Werten der Objekte abgeglichen und
- * <br>              dann im Anschluss komplett ins file geschrieben. Dadurch gehen keine Informationen verloren
- * <br>              Auch Erweiterungen des LigaFiles (z.B, zusätzliche Sektionen wie beim MittellangTeamName
- * <br>              Addon werden erkannt und geschrieben.
- * <br> 20.12.04 2.6 neue Fktion gamesSorted hinzugefügt (Sortierung der Partien)
- * <br> 17.01.05 2.6 SP1 bugFixes und neue html functionen hinzugefügt
- * <br> 01.02.05 2.6 SP1 Mal wieder keyValue gefunden und in keyValues geändert in function strafen()
- * <br> 16.02.05 2.7 Klasse Partie fkt. gToreString() und hToreString() überarbeitet. Bei greenTable wird 0 bzw. 0*
- *									 zurückgegeben.
- * <br> 10.04.05 2.7 (Dank an Gowi) update Funktionen für addons implementiert
  * @author    Tim Schumacher <webobjects@gmx.net>
  * @package classLib
  * @access public
  * @version 2.7
-*/
+ */
 
 require_once(dirname(__FILE__).'/../../init.php');
 // classlib Dateien
@@ -67,10 +26,18 @@ require_once(PATH_TO_ADDONDIR."/classlib/classes/pdf/class.ezpdf.php");
 if (file_exists(PATH_TO_ADDONDIR."/classlib/classes/phpthumb/phpthumb.class.php") ){
 	require_once(PATH_TO_ADDONDIR."/classlib/classes/phpthumb/phpthumb.class.php");
 }
-if (!defined('CLASSLIB_VERSION_NR')) define('CLASSLIB_VERSION_NR','2.7');
-if (!defined('CLASSLIB_VERSION')) define('CLASSLIB_VERSION',' (classlib&nbsp;'.CLASSLIB_VERSION_NR.')');
-if (!defined('CLASSLIB_IMG_TYPES')) define('CLASSLIB_IMG_TYPES',$classlib_img_types);
-if (!defined('CLASSLIB_INFO')) define('CLASSLIB_INFO',"Classlib ".CLASSLIB_VERSION_NR." &#169; <a href=\"mailto:webobjects@gmx.net?subject=LMO-KLASSENBIBLIOTHEK\" title=\"Send mail\">Timme</a>");
+if (!defined('CLASSLIB_VERSION_NR')) {
+  define('CLASSLIB_VERSION_NR','2.7');
+}
+if (!defined('CLASSLIB_VERSION')) {
+  define('CLASSLIB_VERSION',' (classlib&nbsp;'.CLASSLIB_VERSION_NR.')');
+}
+if (!defined('CLASSLIB_IMG_TYPES')) {
+  define('CLASSLIB_IMG_TYPES',$classlib_img_types);
+}
+if (!defined('CLASSLIB_INFO')) {
+  define('CLASSLIB_INFO',"Classlib ".CLASSLIB_VERSION_NR." &#169; <a href=\"mailto:webobjects@gmx.net?subject=LMO-KLASSENBIBLIOTHEK\" title=\"Send mail\">Timme</a>");
+}
 
 
 
