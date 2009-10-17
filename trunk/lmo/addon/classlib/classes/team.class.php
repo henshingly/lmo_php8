845c8e73c55b284cd88db01b146d71c6faefc522
jokerlmo <jokerlmo@2f75fcbe-9628-0410-82d3-d6d6603a2bc3>
2009-10-17 12:05:55 +0000

    white space & indentation fixes added some phpdocs

--- trunk/lmo/addon/classlib/classes/team.class.php
+++ trunk/lmo/addon/classlib/classes/team.class.php
@@ -1,69 +1,84 @@
 <?
 /**
- * Team
- *
- * Mannschaft (Team), die an einer Liga teilnimmt
- *
- * @package   classLib
-*/
+   * Team
+   *
+   * Mannschaft (Team), die an einer Liga teilnimmt
+   *
+   * @package   classLib
+   * @version $Id$
+   */
 
 class team {
-/**
- * Name der Mannschaft
- * @var string
- *
-*/
+  /**
+   * Name der Mannschaft
+   * @var string
+   *
+   */
   var $name;
-/**
- * Kurzbezeichnung (max.5 Zeichen)
- * @var string
- *
-*/
+
+  /**
+   * Kurzbezeichnung (max.5 Zeichen)
+   * @var string
+   *
+   */
   var $kurz;
-/**
- * Mittellanger Teamname
- * @var string
- *
-*/
+
+  /**
+   * Mittellanger Teamname
+   * @var string
+   *
+   */
   var $mittel;
-/**
- * Nummer der Mannschaft, entspricht der Nr aus dem Ligafile
- * @var integer
- *
-*/
+
+  /**
+   * Nummer der Mannschaft, entspricht der Nr aus dem Ligafile
+   * @var integer
+   *
+   */
   var $nr;
-/**
- * KeyValue Paare, die in der Team Sektion angegeben wurden.
- *
- * Beispiel:
- * In einem LigaFile existiert in der Sektion [Team2] der Eintrag URL=http://www.hsv.de
- * Um die URL des Teams anzusprechen schreibt man: $url = $myTeam->keyValues['URL'];
- *
- * @var array
-*/
+
+  /**
+   * KeyValue Paare, die in der Team Sektion angegeben wurden.
+   *
+   * Beispiel:
+   * In einem LigaFile existiert in der Sektion [Team2] der Eintrag URL=http://www.hsv.de
+   * Um die URL des Teams anzusprechen schreibt man: $url = $myTeam->keyValues['URL'];
+   *
+   * @var array
+   */
   var $keyValues;
 
+  /**
+   * Konstruktor
+   *
+   * @param string $name
+   * @param string $kurz
+   * @param string $nr
+   * @param string $mittel
+   * @return team
+   */
   function team($name="",$kurz="",$nr="",$mittel="") {
     $this->name = $name;
     $this->kurz = $kurz;
-    $this->mittel = $mittel; 
+    $this->mittel = $mittel;
     $this->nr = $nr;
     $this->keyValues = array("SP"=>0,"SM"=>0,"TOR1"=>0,"TOR2"=>0,"STDA"=>0,"URL"=>"","NOT"=>"");
   }
 
-/**
- * Fügt ein neues KeyValue Paar hinzu
- *
- * @access private
-*/
+  /**
+   * Fügt ein neues KeyValue Paar hinzu
+   *
+   * @access private
+   */
   function addKeyValue ($new_key,$new_value) {
     $this->keyValues[$new_key]=$new_value;
   }
-/**
- * Gibt value zu einem Key zurück
- *
- * @access private
-*/
+
+  /**
+   * Gibt value zu einem Key zurück
+   *
+   * @access private
+   */
   function valueForKey($new_key) {
     return $this->keyValues[$new_key];
   }
