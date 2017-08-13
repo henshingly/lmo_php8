fcbab9f51bac61440a6cebc29ce2a0bab46915ec
Henshingly <henshingly@vest-sport.de>
2017-08-13 17:17:57 +0200

    Verbesserungsvorschlag
    https://www.liga-manager-online.de/lmo-forum/viewtopic.php?f=5&t=20402&p=78835#p78700

--- lmo/includes/IT.php
+++ lmo/includes/IT.php
@@ -951,7 +951,7 @@ class HTML_Template_IT {
         fclose($fh);
 
         return preg_replace(
-            "#<!-- INCLUDE (.*) -->#ime", "\$this->getFile('\\1')", $content
+            "#<!-- INCLUDE (.*) -->#im", "\$this->getFile('\\1')", $content
         );
     } // end func getFile
 
