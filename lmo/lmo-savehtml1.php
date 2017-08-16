39de4f73f5d01635e1b2dbd99a30ce8b416a6f20
Henshingly <henshingly@vest-sport.de>
2017-08-16 14:58:58 +0200

    Update lmo-savehtml1.php
    Nun eine saubere HTML Ausgabe

--- lmo/lmo-savehtml1.php
+++ lmo/lmo-savehtml1.php
@@ -58,79 +58,95 @@ if($lmtype==0){
   <h1><?php echo $titel?></h1>
   <table>
     <tr>
-      <td><?php
-		for ($y1=1;$y1<$anzst+1;$y1++) {
+      <td>
+<?php
+    for ($y1=1;$y1<$anzst+1;$y1++) {
       $datumanz=$y1-1;
       $z=array_filter($teama[$y1-1],"filterZero");
-      if (!empty($z)) {?>
-    <table>
-      <caption><?php echo $y1?>.  <?php echo $text[2]?><?php if ($datum1[$datumanz]!='') { echo ' - '.$datum1[$datumanz].' '.$text[4].' '.$datum2[$datumanz];}?></caption><?php
+      if (!empty($z)) {
+?>
+        <table>
+        <caption><?php echo $y1?>.  <?php echo $text[2]?><?php if ($datum1[$datumanz]!='') { echo ' - '.$datum1[$datumanz].' '.$text[4].' '.$datum2[$datumanz];}?></caption>
+<?php
         $datsort= $mterm[$y1-1];
         asort($datsort);
         reset($datsort);
         while (list ($key, $val) = each ($datsort)) {
         $i1=$key;
         if(($teama[$y1-1][$i1]>0) && ($teamb[$y1-1][$i1]>0)){
-    			$heimteam=$teams[$teama[$y1-1][$i1]];
-    			$gastteam=$teams[$teamb[$y1-1][$i1]];
-    			$heimtore=applyFactor($goala[$y1-1][$i1],$goalfaktor);
-    			$gasttore=applyFactor($goalb[$y1-1][$i1],$goalfaktor);
-    			if ($gasttore<0) $gasttore="_";
-    			if ($heimtore<0) $heimtore="_";
-    			// * Spielfrei-Hack-Beginn1	- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-    			if (($anzteams-($anzst/2+1))!=0){
-      			$spielfreiaa[$i1]=$teama[$y1-1][$i1];
-      			$spielfreibb[$i1]=$teamb[$y1-1][$i1];
-    			}
-    			// * Spielfrei-Hack-Ende1- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-    			if($mterm[$y1-1][$i1]>0){$dum1=strftime($datf, $mterm[$y1-1][$i1]);}else{$dum1="";} // Anstosszeit einblenden
-			?>
- 	   <tr>
-       <td><?php echo $dum1?>&nbsp;</td>
-       <td align="right"><?php echo $heimteam?></td><td><?php echo HTML_icon($heimteam, 'teams');?></td>
-       <td>-</td>
-       <td><?php echo HTML_icon($gastteam, 'teams');?></td><td><?php echo $gastteam?>&nbsp;</td>
-       <td align="right"><?php echo $heimtore?></td>
-       <td>:</td>
-       <td align="left"><?php echo $gasttore?></td><?php
-      if ($msieg[$y1-1][$i1]==3){ ?>
-        <td width="2">/</td>
-        <td align="right"><?php echo $gasttore?></td>
-        <td align="center" width="8">:</td>
-        <td align="left"><?php echo $heimtore?></td><?php
-      }?>
-     </tr><?php
-		  }		}
-		  $actual=$actual+1;
-		}
-    if (!empty($z)) {?>
-	</table>
-  <?php }
-    if (($anzteams-($anzst/2+1))!=0){
-			$spielfreicc=array_merge($spielfreiaa,$spielfreibb);
-			unset($spielfreiaa);
-			unset($spielfreibb);
-			$i=1;
-			for ($j=1;$j<$anzteams+1;$j++) {
-			if (!in_array($j,$spielfreicc)) {
-			  if ($i==1) {?>
-				<p><small><?php echo $text[4004]?>: <?php
+          $heimteam=$teams[$teama[$y1-1][$i1]];
+          $gastteam=$teams[$teamb[$y1-1][$i1]];
+          $heimtore=applyFactor($goala[$y1-1][$i1],$goalfaktor);
+          $gasttore=applyFactor($goalb[$y1-1][$i1],$goalfaktor);
+          if ($gasttore<0) $gasttore="_";
+          if ($heimtore<0) $heimtore="_";
+          // * Spielfrei-Hack-Beginn1	- Autor: Bernd Hoyer - eMail: info@salzland-info.de
+          if (($anzteams-($anzst/2+1))!=0){
+            $spielfreiaa[$i1]=$teama[$y1-1][$i1];
+            $spielfreibb[$i1]=$teamb[$y1-1][$i1];
+          }
+          // * Spielfrei-Hack-Ende1- Autor: Bernd Hoyer - eMail: info@salzland-info.de
+          if($mterm[$y1-1][$i1]>0){$dum1=strftime($datf, $mterm[$y1-1][$i1]);}else{$dum1="";} // Anstosszeit einblenden
+?>
+          <tr>
+            <td><?php echo $dum1?>&nbsp;</td>
+            <td align="right"><?php echo $heimteam?></td>
+            <td><?php echo HTML_icon($heimteam, 'teams');?></td>
+            <td>-</td>
+            <td><?php echo HTML_icon($gastteam, 'teams');?></td>
+            <td><?php echo $gastteam?>&nbsp;</td>
+            <td align="right"><?php echo $heimtore?></td>
+            <td>:</td>
+            <td align="left"><?php echo $gasttore?></td>
+<?php
+          if ($msieg[$y1-1][$i1]==3){ 
+?>
+            <td width="2">/</td>
+            <td align="right"><?php echo $gasttore?></td>
+            <td align="center" width="8">:</td>
+            <td align="left"><?php echo $heimtore?></td>
+<?php
+          }
+?>
+          </tr>
+<?php
         }
-        echo $teams[$j]?>&nbsp;&nbsp;<?php
-			  $i++;
-		  }
-		}?>
-	</small></p><?php
-		unset($spielfreicc);
-	}
- }
-  $datumanz=$actual-1;?>
-  </td>
-  </tr>
-</table>
-	<table id="tabelle">
-    <caption><?php echo $text[16]?></caption>
- 		<tr>
+      }
+      $actual=$actual+1;
+    }
+    if (!empty($z)) {
+?>
+        </table>
+<?php
+    }
+    if (($anzteams-($anzst/2+1))!=0){
+      $spielfreicc=array_merge($spielfreiaa,$spielfreibb);
+      unset($spielfreiaa);
+      unset($spielfreibb);
+      $i=1;
+      for ($j=1;$j<$anzteams+1;$j++) {
+        if (!in_array($j,$spielfreicc)) {
+          if ($i==1) {?>
+      <p><small><?php echo $text[4004]?>: <?php
+          }
+          echo $teams[$j]?>&nbsp;&nbsp;<?php
+          $i++;
+        }
+      }
+?>
+      </small></p>
+<?php
+      unset($spielfreicc);
+    }
+  }
+  $datumanz=$actual-1;
+?>
+      </td>
+    </tr>
+  </table>
+  <table id="tabelle">
+  <caption><?php echo $text[16]?></caption>
+    <tr>
       <th>&nbsp;</th>
       <th>&nbsp;</th>
       <th>&nbsp;</th>
@@ -139,37 +155,41 @@ if($lmtype==0){
       <th>&nbsp;</th>
       <th><?php echo $nametor?>&nbsp;</th>
       <th align="right" >&nbsp;&nbsp;<?php echo $text[39]?></th>
-    </tr><?php
-
-    for ($i1=0;$i1<$anzteams;$i1++){
-			$platz=$i1+1;
-			$i4=(int)substr($table1[$i1],35,7);
-			$teamname=$teams[$i4];
-			$pluspunkte=applyFactor($punkte[$i4],$pointsfaktor);
-			$minuspunkte=applyFactor($negativ[$i4],$pointsfaktor);
-			$kegelnholz=applyFactor($dtore[$i4],$goalfaktor);
-			$plustore=applyFactor($etore[$i4],$goalfaktor);
-			$minustore=applyFactor($atore[$i4],$goalfaktor);
-			$torverhaeltnis=applyFactor($dtore[$i4],$goalfaktor);
-			$spieleteam=$spiele[$i4];?>
-
-		<tr>
-       <td align="right"><?php echo $platz?>&nbsp;</td>
-	   <td><?php echo HTML_icon($teamname, 'teams');?>&nbsp;</td>
-       <td><?php echo $teamname?>&nbsp;</td>
-       <td align="right"><?php echo $spieleteam?>&nbsp;</td>
-       <td align="right"><?php echo $pluspunkte?>
- 	   <?php if ($minus==2) {
+    </tr>
+<?php
+  for ($i1=0;$i1<$anzteams;$i1++){
+    $platz=$i1+1;
+    $i4=(int)substr($table1[$i1],35,7);
+    $teamname=$teams[$i4];
+    $pluspunkte=applyFactor($punkte[$i4],$pointsfaktor);
+    $minuspunkte=applyFactor($negativ[$i4],$pointsfaktor);
+    $kegelnholz=applyFactor($dtore[$i4],$goalfaktor);
+    $plustore=applyFactor($etore[$i4],$goalfaktor);
+    $minustore=applyFactor($atore[$i4],$goalfaktor);
+    $torverhaeltnis=applyFactor($dtore[$i4],$goalfaktor);
+    $spieleteam=$spiele[$i4];
+?>
+    <tr>
+      <td align="right"><?php echo $platz?>&nbsp;</td>
+      <td><?php echo HTML_icon($teamname, 'teams');?>&nbsp;</td>
+      <td><?php echo $teamname?>&nbsp;</td>
+      <td align="right"><?php echo $spieleteam?>&nbsp;</td>
+      <td align="right"><?php echo $pluspunkte?>
+<?php
+    if ($minus==2) {
       ?>:</td>
-        <td align="left"><?php echo $minuspunkte?>&nbsp;<?php
- 			}else{?>
- 			 </td>
-        <td align="left">&nbsp;<?php
- 			}?>
- 			 <td align="right"><?php echo "$plustore:$minustore"?>&nbsp;</td>
-        <td align="right">&nbsp;&nbsp;<?php echo $torverhaeltnis?></td>
- 		 </tr><?php
-		}?>
+      <td align="left"><?php echo $minuspunkte?>&nbsp;<?php
+    } else {?>
+      </td>
+      <td align="left">&nbsp;<?php
+    }
+?>
+      <td align="right"><?php echo "$plustore:$minustore"?>&nbsp;</td>
+      <td align="right">&nbsp;&nbsp;<?php echo $torverhaeltnis?></td>
+    </tr>
+<?php
+  }
+?>
   </table>
   <script type="text/javascript">document.write('<small><a href="#" onClick="history.back();return false;"><?php echo $text[562]?><\/a><\/small>');</script>
 </body>
