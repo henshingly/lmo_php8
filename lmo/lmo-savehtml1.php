8a016e17c0d69954c4f07265da09759c4d15ff15
Henshingly <henshingly@vest-sport.de>
2017-08-16 15:13:53 +0200

    Update lmo-savehtml1.php
    Tabulatoren gegen Leerzeichen entfernt

--- lmo/lmo-savehtml1.php
+++ lmo/lmo-savehtml1.php
@@ -17,40 +17,40 @@
 
 if($st>0){$actual=$anzst;}else{$actual=$stx;}
 if($lmtype==0){
-	for($i1=0;$i1<$anzsp;$i1++){
-		if ($goala[$actual-1][$i1]=="-1") $goala[$actual-1][$i1]="_";
-		if ($goalb[$actual-1][$i1]=="-1") $goalb[$actual-1][$i1]="_";
-	}
-	$endtab=$actual;
-	include_once(PATH_TO_LMO."/lmo-calctable.php");
+  for($i1=0;$i1<$anzsp;$i1++){
+    if ($goala[$actual-1][$i1]=="-1") $goala[$actual-1][$i1]="_";
+    if ($goalb[$actual-1][$i1]=="-1") $goalb[$actual-1][$i1]="_";
+  }
+  $endtab=$actual;
+  include_once(PATH_TO_LMO."/lmo-calctable.php");
 
-	for($i1=0;$i1<$anzsp;$i1++){
-		if ($goala[$actual-1][$i1]=="_") $goala[$actual-1][$i1]="-1";
-		if ($goalb[$actual-1][$i1]=="_") $goalb[$actual-1][$i1]="-1";
-	}
+  for($i1=0;$i1<$anzsp;$i1++){
+    if ($goala[$actual-1][$i1]=="_") $goala[$actual-1][$i1]="-1";
+    if ($goalb[$actual-1][$i1]=="_") $goalb[$actual-1][$i1]="-1";
+  }
 }
 if($lmtype==0){
-	isset($tab0) ? $table1=$tab0 : $table1=$tab1;
+  isset($tab0) ? $table1=$tab0 : $table1=$tab1;
   if (isset($table1)) {
     $wmlfile= fopen(PATH_TO_LMO.'/'.$diroutput.basename($file)."-sp.html","wb");
-		ob_start();
-		?>
+    ob_start();
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-					"http://www.w3.org/TR/html4/loose.dtd">
+        "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
   <title><?php echo $titel?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
   <style type="text/css">
-   body {background:#fff; color:#000; font: 10pt sans-serif;padding:auto;}
-   caption, p, h1 {margin: 3pt auto; text-align:center;}
-   table {margin:auto; max-width:200mm;}
-   #tabelle{border:1pt solid #000;}
-   table table {border:1pt solid #000; margin:20pt 0 2pt; width:100%;}
-   td {padding: 0; white-space:nowrap;}
-   th {border-bottom: 1px solid #000;}
-   h1 {font:14pt bolder;}
-   caption {font-weight:bolder;white-space:nowrap;}
+    body {background:#fff; color:#000; font: 10pt sans-serif;padding:auto;}
+    caption, p, h1 {margin: 3pt auto; text-align:center;}
+    table {margin:auto; max-width:200mm;}
+    #tabelle{border:1pt solid #000;}
+    table table {border:1pt solid #000; margin:20pt 0 2pt; width:100%;}
+    td {padding: 0; white-space:nowrap;}
+    th {border-bottom: 1px solid #000;}
+    h1 {font:14pt bolder;}
+    caption {font-weight:bolder;white-space:nowrap;}
   </style>
 </head>
 <body>
@@ -72,21 +72,21 @@ if($lmtype==0){
         asort($datsort);
         reset($datsort);
         while (list ($key, $val) = each ($datsort)) {
-        $i1=$key;
-        if(($teama[$y1-1][$i1]>0) && ($teamb[$y1-1][$i1]>0)){
-          $heimteam=$teams[$teama[$y1-1][$i1]];
-          $gastteam=$teams[$teamb[$y1-1][$i1]];
-          $heimtore=applyFactor($goala[$y1-1][$i1],$goalfaktor);
-          $gasttore=applyFactor($goalb[$y1-1][$i1],$goalfaktor);
-          if ($gasttore<0) $gasttore="_";
-          if ($heimtore<0) $heimtore="_";
-          // * Spielfrei-Hack-Beginn1	- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-          if (($anzteams-($anzst/2+1))!=0){
-            $spielfreiaa[$i1]=$teama[$y1-1][$i1];
-            $spielfreibb[$i1]=$teamb[$y1-1][$i1];
-          }
-          // * Spielfrei-Hack-Ende1- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-          if($mterm[$y1-1][$i1]>0){$dum1=strftime($datf, $mterm[$y1-1][$i1]);}else{$dum1="";} // Anstosszeit einblenden
+          $i1=$key;
+          if(($teama[$y1-1][$i1]>0) && ($teamb[$y1-1][$i1]>0)){
+            $heimteam=$teams[$teama[$y1-1][$i1]];
+            $gastteam=$teams[$teamb[$y1-1][$i1]];
+            $heimtore=applyFactor($goala[$y1-1][$i1],$goalfaktor);
+            $gasttore=applyFactor($goalb[$y1-1][$i1],$goalfaktor);
+            if ($gasttore<0) $gasttore="_";
+            if ($heimtore<0) $heimtore="_";
+            // * Spielfrei-Hack-Beginn1	- Autor: Bernd Hoyer - eMail: info@salzland-info.de
+            if (($anzteams-($anzst/2+1))!=0){
+              $spielfreiaa[$i1]=$teama[$y1-1][$i1];
+              $spielfreibb[$i1]=$teamb[$y1-1][$i1];
+            }
+            // * Spielfrei-Hack-Ende1- Autor: Bernd Hoyer - eMail: info@salzland-info.de
+            if($mterm[$y1-1][$i1]>0){$dum1=strftime($datf, $mterm[$y1-1][$i1]);}else{$dum1="";} // Anstosszeit einblenden
 ?>
           <tr>
             <td><?php echo $dum1?>&nbsp;</td>
@@ -99,47 +99,47 @@ if($lmtype==0){
             <td>:</td>
             <td align="left"><?php echo $gasttore?></td>
 <?php
-          if ($msieg[$y1-1][$i1]==3){ 
+            if ($msieg[$y1-1][$i1]==3){
 ?>
             <td width="2">/</td>
             <td align="right"><?php echo $gasttore?></td>
             <td align="center" width="8">:</td>
             <td align="left"><?php echo $heimtore?></td>
 <?php
-          }
+            }
 ?>
           </tr>
 <?php
+          }
         }
+        $actual=$actual+1;
       }
-      $actual=$actual+1;
-    }
-    if (!empty($z)) {
+      if (!empty($z)) {
 ?>
         </table>
 <?php
-    }
-    if (($anzteams-($anzst/2+1))!=0){
-      $spielfreicc=array_merge($spielfreiaa,$spielfreibb);
-      unset($spielfreiaa);
-      unset($spielfreibb);
-      $i=1;
-      for ($j=1;$j<$anzteams+1;$j++) {
-        if (!in_array($j,$spielfreicc)) {
-          if ($i==1) {?>
-      <p><small><?php echo $text[4004]?>: <?php
-          }
-          echo $teams[$j]?>&nbsp;&nbsp;<?php
-          $i++;
-        }
       }
+      if (($anzteams-($anzst/2+1))!=0){
+        $spielfreicc=array_merge($spielfreiaa,$spielfreibb);
+        unset($spielfreiaa);
+        unset($spielfreibb);
+        $i=1;
+        for ($j=1;$j<$anzteams+1;$j++) {
+          if (!in_array($j,$spielfreicc)) {
+            if ($i==1) {?>
+      <p><small><?php echo $text[4004]?>: <?php
+            }
+            echo $teams[$j]?>&nbsp;&nbsp;<?php
+            $i++;
+          }
+        }
 ?>
       </small></p>
 <?php
-      unset($spielfreicc);
+        unset($spielfreicc);
+      }
     }
-  }
-  $datumanz=$actual-1;
+    $datumanz=$actual-1;
 ?>
       </td>
     </tr>
@@ -157,17 +157,17 @@ if($lmtype==0){
       <th align="right" >&nbsp;&nbsp;<?php echo $text[39]?></th>
     </tr>
 <?php
-  for ($i1=0;$i1<$anzteams;$i1++){
-    $platz=$i1+1;
-    $i4=(int)substr($table1[$i1],35,7);
-    $teamname=$teams[$i4];
-    $pluspunkte=applyFactor($punkte[$i4],$pointsfaktor);
-    $minuspunkte=applyFactor($negativ[$i4],$pointsfaktor);
-    $kegelnholz=applyFactor($dtore[$i4],$goalfaktor);
-    $plustore=applyFactor($etore[$i4],$goalfaktor);
-    $minustore=applyFactor($atore[$i4],$goalfaktor);
-    $torverhaeltnis=applyFactor($dtore[$i4],$goalfaktor);
-    $spieleteam=$spiele[$i4];
+    for ($i1=0;$i1<$anzteams;$i1++){
+      $platz=$i1+1;
+      $i4=(int)substr($table1[$i1],35,7);
+      $teamname=$teams[$i4];
+      $pluspunkte=applyFactor($punkte[$i4],$pointsfaktor);
+      $minuspunkte=applyFactor($negativ[$i4],$pointsfaktor);
+      $kegelnholz=applyFactor($dtore[$i4],$goalfaktor);
+      $plustore=applyFactor($etore[$i4],$goalfaktor);
+      $minustore=applyFactor($atore[$i4],$goalfaktor);
+      $torverhaeltnis=applyFactor($dtore[$i4],$goalfaktor);
+      $spieleteam=$spiele[$i4];
 ?>
     <tr>
       <td align="right"><?php echo $platz?>&nbsp;</td>
@@ -176,24 +176,25 @@ if($lmtype==0){
       <td align="right"><?php echo $spieleteam?>&nbsp;</td>
       <td align="right"><?php echo $pluspunkte?>
 <?php
-    if ($minus==2) {
-      ?>:</td>
+      if ($minus==2) {?>:</td>
       <td align="left"><?php echo $minuspunkte?>&nbsp;<?php
-    } else {?>
+      } else {?>
       </td>
       <td align="left">&nbsp;<?php
-    }
+      }
 ?>
       <td align="right"><?php echo "$plustore:$minustore"?>&nbsp;</td>
       <td align="right">&nbsp;&nbsp;<?php echo $torverhaeltnis?></td>
     </tr>
 <?php
-  }
+    }
 ?>
   </table>
   <script type="text/javascript">document.write('<small><a href="#" onClick="history.back();return false;"><?php echo $text[562]?><\/a><\/small>');</script>
 </body>
-</html><?php }
+</html>
+<?php
+  }
     fwrite($wmlfile,ob_get_contents());
     ob_end_clean();
     fclose($wmlfile);
