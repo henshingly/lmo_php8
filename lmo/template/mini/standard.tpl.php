22509886b1413b763a02f5aef4ec35acd75639d7
henshingly <henshingly@vest-sport.de>
2017-09-19 15:13:28 +0200

    TendenzIMG in der Tabelle mit aufgenommen
    Signed-off-by: henshingly <henshingly@vest-sport.de>

--- lmo/template/mini/standard.tpl.php
+++ lmo/template/mini/standard.tpl.php
@@ -1,8 +1,9 @@
 <table style="font-size:90%;font-family:sans-serif;border-spacing:0;border-collapse:collapse;border:1px solid black;" cellspacing="0">
-  <caption style="font-weight:bold;border:1px solid black;"><a style="text-decoration:underline;" href="<!--Link-->">Tabelle »</a></caption>
+  <caption style="font-weight:bold;border:1px solid black;"><a style="text-decoration:underline;" href="<!--Link-->"><!--Tabelle--> Â»</a></caption>
   <!-- BEGIN Inhalt -->
   <tr style="<!--Style-->">
     <td align="right"><!--Platz--></td>
+    <td align="right"><!--TendenzIMG--></td>
     <td align="center"><!--TeamBild--></td>
     <td><acronym title="<!--TeamLang-->"><!--Team--></acronym></td>
     <td align="right"><!--Punkte--></td>
