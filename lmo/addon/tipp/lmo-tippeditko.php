a3647d23dac259484f61a3d3f5d304251e6fbe59
Henshingly <henshingly@vest-sport.de>
2018-11-29 13:34:16 +0100

    Update lmo-tippeditko.php
    mysql_escape_string ()
    Diese Funktion wurde in PHP 4.3.0 als veraltet gekennzeichnet und wurde in PHP 7.0.0 zusammen mit der gesamten ursprünglichen MySQL-Erweiterung entfernt.

--- lmo/addon/tipp/lmo-tippeditko.php
+++ lmo/addon/tipp/lmo-tippeditko.php
@@ -289,7 +289,7 @@ for($n=0;$n<$modus[$st-1];$n++){
    */
   if($urlb==1){
     if($mberi[$st-1][$i][$n]!=""){
-      $lmo_spielbericht="<strong>".$teams[$teama[$st-1][$i]]."</strong>".$lmo_teamaicon." - ".$lmo_teambicon."<strong>".$teams[$teamb[$st-1][$i]]."</strong><br><br>";
+      $lmo_spielbericht=$lmo_teamaicon."<strong>".$teams[$teama[$st-1][$i]]."</strong> - ".$lmo_teambicon."<strong>".$teams[$teamb[$st-1][$i]]."</strong><br><br>";
       echo "<a href='".$mberi[$st-1][$i][$n]."'  target='_blank' title='".$text[270]."'><img src='".URL_TO_IMGDIR."/lmo-st1.gif' width='10' height='12' border='0' alt=''><span class='popup'>".$lmo_spielbericht.nl2br($text[270])."</span></a>";
     }else{
       echo "&nbsp;&nbsp;&nbsp;";
@@ -300,18 +300,16 @@ for($n=0;$n<$modus[$st-1];$n++){
    */
   if ($mnote[$st-1][$i][$n]!="" || $mtipp[$st-1][$i][$n] > 0) {
 
-    $lmo_spielnotiz="<strong>".$teams[$teama[$st-1][$i]]."</strong>".$lmo_teamaicon." - ".$lmo_teambicon."<strong>".$teams[$teamb[$st-1][$i]]."</strong> ".$goala[$st-1][$i][$n].":".$goalb[$st-1][$i][$n];
+    $lmo_spielnotiz=$lmo_teamaicon."<strong>".$teams[$teama[$st-1][$i]]."</strong> - ".$lmo_teambicon."<strong>".$teams[$teamb[$st-1][$i]]."</strong> ".$goala[$st-1][$i][$n].":".$goalb[$st-1][$i][$n];
     //Allgemeine Notiz
     
-    //$lmo_spielnotiz.="\n\n<strong>".$text[22].":</strong> ".$mnote[$st-1][$i][$n];
+    $lmo_spielnotiz.="\n\n<strong>".$text[22].":</strong> ".$mnote[$st-1][$i][$n];
     
     if ($mtipp[$st-1][$i][$n] == 1) {
-      $alert=htmlentities(strip_tags($lmo_spielnotiz))."  -  ".$text['tipp'][231];
       $lmo_spielnotiz.="\n\n".$text['tipp'][231];
     }
-    echo "<a href='#' onclick=\"alert('".$alert."');window.focus();return false;\"><span class='popup'>".nl2br($lmo_spielnotiz)."</span><img src='".URL_TO_IMGDIR."/lmo-st2.gif' width='10' height='12' border='0' alt=''></a>";
+    echo "<a href='#' onclick=\"alert('".mysql_escape_string(htmlentities(strip_tags($lmo_spielnotiz)))."');window.focus();return false;\"><span class='popup'>".nl2br($lmo_spielnotiz)."</span><img src='".URL_TO_IMGDIR."/lmo-st2.gif' width='10' height='12' border='0' alt=''></a>";
     $lmo_spielnotiz="";
-    $alert="";
   } else {
     echo "&nbsp;";
   }?>
