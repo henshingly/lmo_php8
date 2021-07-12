a7e34966a4a33779a2ca7068c3a8d7f30f4e1a7e
dwblmo <dummwiebrot@gmail.com>
2021-07-12 08:52:59 +0200

    update bs-version

--- lmo/template/lmo-standard-komplett.tpl.php
+++ lmo/template/lmo-standard-komplett.tpl.php
@@ -6,7 +6,7 @@
 <title><!--Titel--></title>
 <!--Stylesheet-->
 <!-- CSS only -->
-<link href="//cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<link href="//cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
 </head>
 <body>
@@ -44,6 +44,12 @@
 	</div>
   </div>
   <!-- JavaScript Bundle with Popper -->
-   <script type="text/javascrpt" src="//cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+   <script type="text/javascript" src="//cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+   <script type="text/javascript">
+      var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+      var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+          return new bootstrap.Tooltip(tooltipTriggerEl)
+      })
+   </script>
   </body>
 </html>
