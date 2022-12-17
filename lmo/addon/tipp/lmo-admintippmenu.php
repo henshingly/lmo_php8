bfc47d81682ecb4e87c00f461093f7b9815ba1ee
dwblmo <dummwiebrot@gmail.com>
2022-12-17 10:06:41 +0100

    Update lmo-admintippmenu.php
    navigation bootstraped

--- lmo/addon/tipp/lmo-admintippmenu.php
+++ lmo/addon/tipp/lmo-admintippmenu.php
@@ -1,33 +1,33 @@
-<table class="lmoSubmenu" width="99%" cellspacing="0" cellpadding="0" border="0">
-  <tr>
-    <td align="center"><?php 
+<nav>
+  <ul class="nav nav-pills justify-content-center">
+    <li class="nav-item"><?php 
 if ($todo!='tipp') {?>
-      <a href='<?php echo $tipp_addr_auswertung?>' onclick="return chklmolink();" title="<?php echo $text['tipp'][63]?>"><?php echo $text['tipp'][63]?></a><?php 
-} else {
-  echo $text['tipp'][63];
-}?> </td>
-    <td align="center"><?php 
+      <a href='<?php echo $tipp_addr_auswertung?>' class='nav-link' onclick="return chklmolink();" title="<?php echo $text['tipp'][63]?>"><?php echo $text['tipp'][63]?></a><?php 
+} else {?>
+      <a href='#' class='nav-link active' onclick="return chklmolink();" title="<?php echo $text['tipp'][63]?>"><?php echo $text['tipp'][63]?></a><?php
+}?> </li>
+    <li class="nav-item"><?php 
 if($_SESSION['lmouserok']==2){
   if ($todo!='tippemail') {?>
-      <a href='<?php echo $tipp_addr_email?>' onClick="return chklmolink();" title="<?php echo $text['tipp'][165]?>"><?php echo $text['tipp'][165]?></a><?php 
-  } else {
-    echo $text['tipp'][165];
+      <a href='<?php echo $tipp_addr_email?>' class='nav-link' onClick="return chklmolink();" title="<?php echo $text['tipp'][165]?>"><?php echo $text['tipp'][165]?></a><?php 
+  } else {?>
+      <a href='#' class='nav-link active' onClick="return chklmolink();" title="<?php echo $text['tipp'][165]?>"><?php echo $text['tipp'][165]?></a><?php
   }?> 
-    </td>
-    <td align="center"><?php 
+    </li>
+    <li class="nav-item"><?php 
   if ($todo!='tippuser' && $todo!='tippuseredit') {?>
-      <a href='<?php echo $tipp_addr_user?>' onClick="return chklmolink();" title="<?php echo $text['tipp'][114]?>"><?php echo $text['tipp'][114]?></a><?php 
-  } else {
-    echo $text['tipp'][114];
+      <a href='<?php echo $tipp_addr_user?>' class='nav-link' onClick="return chklmolink();" title="<?php echo $text['tipp'][114]?>"><?php echo $text['tipp'][114]?></a><?php 
+  } else {?>
+      <a href='+' class='nav-link active' onClick="return chklmolink();" title="<?php echo $text['tipp'][114]?>"><?php echo $text['tipp'][114]?></a><?php
   }?> 
-    </td>
-    <td align="center"><?php 
+    </li>
+    <li class="nav-item"><?php 
   if ($todo!='tippoptions') {?>
-      <a href='<?php echo $tipp_addr_optionen?>' onClick="return chklmolink();" title="<?php echo $text['tipp'][55]?>"><?php echo $text[86]?></a><?php 
-  } else {
-    echo $text[86];
+      <a href='<?php echo $tipp_addr_optionen?>' class='nav-link' onClick="return chklmolink();" title="<?php echo $text['tipp'][55]?>"><?php echo $text[86]?></a><?php 
+  } else {?>
+      <a href='#' class='nav-link active' onClick="return chklmolink();" title="<?php echo $text['tipp'][55]?>"><?php echo $text[86]?></a><?php
   }?> 
-    </td><?php 
+    </li><?php 
 }?>
-  </tr>
-</table>
+  </ul>
+</nav>
