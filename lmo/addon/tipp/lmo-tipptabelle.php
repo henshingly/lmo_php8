e02cee4e82bfdfbbb020242c7f7d1002ffffbc34
dwblmo <dummwiebrot@gmail.com>
2023-04-30 09:49:13 +0200

    Update lmo-tipptabelle.php
    remove gif

--- lmo/addon/tipp/lmo-tipptabelle.php
+++ lmo/addon/tipp/lmo-tipptabelle.php
@@ -250,8 +250,8 @@ if ($file!="" && $tipp_tipptabelle1==1) {
       //Teamnotizen
       if ($teamn[$i]!="") {
         $lmo_tabellennotiz.="\n\n<strong>".$text[22].":</strong>\n".$teamn[$i];
-      }?>
-        <a href='#' onclick="alert('<?php echo addcslashes('',htmlentities(strip_tags($lmo_tabellennotiz)))?>');window.focus();return false;"><img src='<?php echo URL_TO_IMGDIR."/lmo-st2.gif"?>' width='10' height='12' border='0' alt=''><span class='popup'><?php echo nl2br($lmo_tabellennotiz)?></span></a><?php 
+      }
+      echo "<a data-bs-toggle='tooltip' data-bs-placement='right' data-bs-html='true' title='".$lmo_tabellennotiz."'> <i class='bi bi-info-circle-fill text-info' style='font-size: 1.3rem;'></i></a>";
       $lmo_tabellennotiz="";
     } else {
       echo "";
