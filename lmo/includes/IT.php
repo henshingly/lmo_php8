abb6c4191d70a551e95a1088825c587f007de917
Henshingly <dietmar_kersting@web.de>
2023-07-20 15:12:23 +0200

    Update IT.php
    Classes with #[AllowDynamicProperties] attribute

--- lmo/includes/IT.php
+++ lmo/includes/IT.php
@@ -123,6 +123,9 @@ define('IT_UNKNOWN_OPTION',            -6);
  * @access   public
  * @package  HTML_Template_IT
  */
+ 
+#[\AllowDynamicProperties]
+ 
 class HTML_Template_IT {
 
     /**
@@ -715,18 +718,19 @@ class HTML_Template_IT {
      * @access   public
      * @see      init()
      */
-    function free()
-    {
+    function free() {
+
         $this->err = array();
 
-        $this->currentBlock = '__global__';
+        $this->currentBlock = "__global__";
 
-        $this->variableCache = array();
-        $this->blocklist     = array();
-        $this->touchedBlocks = array();
+        $this->variableCache    = array();
+        $this->blocklookup      = array();
+        $this->touchedBlocks    = array();
 
         $this->flagBlocktrouble = false;
         $this->flagGlobalParsed = false;
+
     } // end func free
 
     /**
