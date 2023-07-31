4b61a68ee49f7507020dbf3854293302c45f721f
Henshingly <dietmar_kersting@web.de>
2023-07-31 16:12:04 +0200

    Update lmo-showgraph.php
    New design for Fevercurves run under PHP 8

--- lmo/lmo-showgraph.php
+++ lmo/lmo-showgraph.php
@@ -7,7 +7,7 @@
   * modify it under the terms of the GNU General Public License as
   * published by the Free Software Foundation; either version 2 of
   * the License, or (at your option) any later version.
-  *
+  * 
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -15,96 +15,149 @@
   *
   * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
   *
-  * $Id$
   */
 
-
-if(($file!="") && ($kurve==1)){
-  $addp=$_SERVER['PHP_SELF']."?action=graph&amp;file=".$file."&amp;stat1=";
-  $show_stat1=isset($_GET['stat1'])?$_GET['stat1']:$stat1;
-  $show_stat2=isset($_GET['stat2'])?$_GET['stat2']:$stat2;
-  if ($show_stat1==0 && $show_stat2!=0 || $show_stat1==$show_stat2) {
-    $show_stat1=$show_stat2;
-    $show_stat2=0;
-  }
+if($_SERVER['PHP_SELF'] == '/lmo-showgraph.php')
+header("Location: /lmo.php");
+  
+if(($file != "") && ($kurve == 1)){
+    $tension = '0.2';
+    $addp=$_SERVER['PHP_SELF']."?action=graph&amp;file=".$file."&amp;stat1=";
+    $show_stat1=isset($_GET['stat1'])?$_GET['stat1']:$stat1;
+    $show_stat2=isset($_GET['stat2'])?$_GET['stat2']:$stat2;
+    if ($show_stat1 == 0 && $show_stat2 != 0 || $show_stat1 == $show_stat2) {
+      $show_stat1 = $show_stat2;
+      $show_stat2 = 0;
+    }
 ?>
 
-<table class="lmoMiddle" cellspacing="0" cellpadding="0" border="0">
-  <tr>
-    <td valign="top" align="center">
-      <table class="lmoMenu" cellspacing="0" cellpadding="0" border="0"><?php
-  for ($i=1; $i<=$anzteams; $i++) {?>
-        <tr>
-          <td align="right">
-            <acronym title="<?php echo $text[134]." ".$teams[$i]?>">
-            	<?php
-    if($i!=$show_stat1){?>
-            <a href="<?php echo $addp.$i?>" ><?php echo $teamk[$i]?></a>
-            <?php
-    } else {
-      echo $teamk[$i];
-    }
-          ?></acronym>
-          </td>
-          <td>&nbsp;<?php echo HTML_smallTeamIcon($file,$teams[$i]," alt=''"); ?>&nbsp;</td>
-        </tr><?php
-  }?>
-      </table>
-    </td>
-    <td valign="top" align="center">
-      <table class="lmoInner" cellspacing="0" cellpadding="0" border="0"><?php
-  if($show_stat1==0){?>
-        <tr>
-          <td>&nbsp;<br /><?php echo $text[24]?><br />&nbsp;</td>
-        </tr><?php
-  } else {
+          <table class="lmoMiddle" cellspacing="0" cellpadding="0" border="0">
+            <tr>
+              <td valign="top" align="center">
+                <table class="lmoInner" cellspacing="0" cellpadding="0" border="0">
+<?php
     $tabtype=0;
-    require(PATH_TO_LMO."/lmo-calcgraph.php");
-    $dummy=URL_TO_LMO."/lmo-paintgraph.php?pganz=";
-    if($show_stat2>0){$dummy=$dummy."2";}else{$dummy=$dummy."1";}
-    $dummy=$dummy."&amp;pgteam1=".rawurlencode($teams[$show_stat1]);
-    if($show_stat2>0){$dummy=$dummy."&amp;pgteam2=".rawurlencode($teams[$show_stat2]);}
-    $dummy=$dummy."&amp;pgteams=".$anzteams;
-    $dummy=$dummy."&amp;pgst=".$anzst;
-    $dummy=$dummy."&amp;pgch=".$champ;
-    $dummy=$dummy."&amp;pgcl=".$anzcl;
-    $dummy=$dummy."&amp;pgck=".$anzck;
-    $dummy=$dummy."&amp;pguc=".$anzuc;
-    $dummy=$dummy."&amp;pgar=".$anzar;
-    $dummy=$dummy."&amp;pgab=".$anzab;
-    $dummy=$dummy."&amp;pgplatz1=";
-    for($j=0;$j<$anzst;$j++){$dummy=$dummy.$platz[$show_stat1][$j].",";}
-    $dummy=$dummy."0";
-    if($show_stat2>0){
-      $dummy=$dummy."&amp;pgplatz2=";
-      for($j=0;$j<$anzst;$j++){$dummy=$dummy.$platz[$show_stat2][$j].",";}
-      $dummy=$dummy."0";
-      }?>
-        <tr>
-          <td align="center"><img src="<?php echo $dummy; ?>" border="0" alt="<?php echo $text[133]?>" /></td>
-        </tr>
-<?php } ?>
-
-  </table></td>
-    <td valign="top" align="center">
-      <table class="lmoMenu" cellspacing="0" cellpadding="0" border="0"><?php
-  for ($i=1; $i<=$anzteams; $i++) {?>
-        <tr>
-          <td>&nbsp;<?php echo HTML_smallTeamIcon($file,$teams[$i]," alt=''"); ?>&nbsp;</td>
-          <td align="right">
-            <acronym title="<?php echo $text[134]." ".$teams[$i]?>"><?php
-    if($i!=$show_stat2){?>
-            <a href="<?php echo $addp.$show_stat1."&amp;stat2=".$i?>" ><?php echo $teamk[$i]?></a><?php
-    } else {
-      echo $teamk[$i];
+    require(PATH_TO_LMO . "/lmo-calcgraph.php");
+    for($k = 1; $k < $anzteams + 1; $k++) {
+        ${'platz_' . $k} = "";
+        for ($l = 0; $l < $anzst; $l++) {
+            ${'platz_' . $k} = ${'platz_' . $k} . $platz[$k][$l] . ",";
+        }
+    }?>
+                  <tr>
+                    <td align="center"><br/><canvas id="myChart" width="800" height="450"></canvas>
+<?php
+    $data = "";
+    $pgtext1 = $text[135];
+    $pgtext2 = $text[136];
+    // Anzahl Spieltage in x-Achse
+    $spieltag = array();
+    for($i = 1; $i <= $anzst; $i++) {
+        $spieltag[] = $i;
     }
-          ?></acronym>
-          </td>
-        </tr><?php
-  }?>
-      </table>
-    </td>
-  </tr>
-</table>
+    $xAxis = implode(", ", $spieltag);
 
-<?php } ?>
+    for($j = 1; $j < $anzteams + 1; $j++) {
+        ${'platz' . $j} = explode(", ", ${'platz_' . $j});
+        foreach(array_keys(${'platz' . $j}, '0') as $key) {
+            unset(${'platz' . $j}[$key]);
+        }
+        ${'pgplatz' . $j} = implode(", ", ${'platz' . $j});
+        $color = mt_rand(0, 240) . ", " . mt_rand(0, 240) . ", " . mt_rand(0, 240) . ", " . mt_rand(0, 240);
+        $axisColor = "rgba(90, 90, 90, 1)";
+        if ($show_stat1 == 0 && $show_stat2 == 0) {
+          $flag = 'true';
+          if($j < 3) $flag = 'false';
+        } else if ($j == $show_stat1 || $j == $show_stat2) {
+            $flag = 'false';
+        } else {
+            $flag = 'true';
+        }
+        $data .= "{	
+                              label: '$teams[$j]',
+                              fill: false,
+                              lineTension: $tension,
+                              backgroundColor: 'rgba($color)',
+                              borderColor: 'rgba($color)',
+                              data: [{${'pgplatz'.$j}}],
+                              hidden: $flag,
+                            },";
+    } ?>
+                      <script src="<?php echo URL_TO_LMO?>/js/chart.js"></script>
+                      <script>
+                        const ctx = document.getElementById('myChart');
+                        new Chart(ctx, {
+                          type: 'line',
+                          data: {
+                            labels: [<?php echo $xAxis; ?>],
+                            datasets: [<?php echo $data; ?>]
+                          },
+                          options: {
+                            scales: {
+                              x: {
+                                grid: {
+                                  color: '<?php echo $axisColor; ?>'
+                                },
+                                title: {
+                                  display: true,
+                                  text: '<?php echo $pgtext1; ?>',
+                                  color: '<?php echo $axisColor; ?>',
+                                  font: {
+                                    size: 24,
+                                  },
+                                },
+                                ticks: {
+                                  color: '<?php echo $axisColor; ?>'
+                                }
+                              },
+                              y: {
+                                grid: {
+                                  color: '<?php echo $axisColor; ?>'
+                                },
+                                title: {
+                                  display: true,
+                                  text: '<?php echo $pgtext2; ?>',
+                                  color: '<?php echo $axisColor; ?>',
+                                  font: {
+                                    size: 24,
+                                  },
+                                },
+                                min: 1,
+                                max: <?php echo $anzteams; ?>,
+                                reverse: true,
+                                ticks: {
+                                  maxTicksLimit: <?php echo $anzteams; ?>,
+                                  color: '<?php echo $axisColor; ?>'
+                                }
+                              },
+                            },
+                            plugins: {
+                              tooltip: {
+                                displayColors: true,
+                                mode: 'index', callbacks: {
+                                  label: function(context) {
+                                    var label = context.dataset.label;
+                                    var pos = "<?php echo $text[577]; ?>: ";
+                                    pos += context.parsed.y;
+                                    return label + " - " + pos;
+                                  },
+                                  title: function(context) {
+                                    var label = context[0].label;
+                                    label += ". <?php echo $text[2]; ?>";
+                                    return label;
+                                  }
+                                }
+                              }
+                            }
+                          }      
+                        });
+                      </script>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        <?php 
+}
+?>
