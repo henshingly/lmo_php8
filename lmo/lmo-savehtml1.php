2963da34bb35c71e3ab7ca9243948c254630a4cb
Henshingly <dietmar_kersting@web.de>
2023-08-01 14:31:50 +0200

    Update
    Edition of the date view renewed. Improved source code view.

--- lmo/lmo-savehtml1.php
+++ lmo/lmo-savehtml1.php
@@ -1,39 +1,47 @@
 <?php
 /**
- * lmo-savehtml1.php: HTML-Ausgabe von Tabelle, aktuellem Spieltag und folgenden Spieltag
- * In der Datei lmo-savefile.php muss über der Zeile
- *  $datei = fopen($file,"w");
- *
- * folgende Zeile hinzugefügt werden:
- *
- *  include(PATH_TO_LMO."/lmo-savehtml1.php");
- *
- *
- * Autor: Bernd Hoyer, basierend auf dem LMO3.02
- * Verbesserungen, Bugs etc. bitte nur in das Forum bei Hollwitz.net
- *
- */
+  * lmo-savehtml1.php: HTML-Ausgabe von Tabelle, aktuellem Spieltag und folgenden Spieltag
+  * In der Datei lmo-savefile.php muss über der Zeile
+  *  $datei = fopen($file,"w");
+  *
+  * folgende Zeile hinzugefügt werden:
+  *
+  *  include(PATH_TO_LMO."/lmo-savehtml1.php");
+  *
+  *
+  * Autor: Bernd Hoyer, basierend auf dem LMO3.02
+  * Verbesserungen, Bugs etc. bitte nur in das Forum bei Hollwitz.net
+  *
+  * 01.08.2023 - Verbesserungen bitte nun in das Forum auf vest-sport.de/forum
+  *
+  *
+  */
 
 
-if($st>0){$actual=$anzst;}else{$actual=$stx;}
-if($lmtype==0){
-  for($i1=0;$i1<$anzsp;$i1++){
-    if ($goala[$actual-1][$i1]=="-1") $goala[$actual-1][$i1]="_";
-    if ($goalb[$actual-1][$i1]=="-1") $goalb[$actual-1][$i1]="_";
-  }
-  $endtab=$actual;
-  include_once(PATH_TO_LMO."/lmo-calctable.php");
-
-  for($i1=0;$i1<$anzsp;$i1++){
-    if ($goala[$actual-1][$i1]=="_") $goala[$actual-1][$i1]="-1";
-    if ($goalb[$actual-1][$i1]=="_") $goalb[$actual-1][$i1]="-1";
-  }
+if ($st > 0) {
+    $actual = $anzst;
+} else {
+    $actual = $stx;
 }
-if($lmtype==0){
-  isset($tab0) ? $table1=$tab0 : $table1=$tab1;
-  if (isset($table1)) {
-    $wmlfile= fopen(PATH_TO_LMO.'/'.$diroutput.basename($file)."-sp.html","wb");
-    ob_start();
+
+if ($lmtype == 0) {
+    for($i1 = 0; $i1 < $anzsp; $i1++) {
+        if ($goala[$actual - 1][$i1] == "-1") $goala[$actual - 1][$i1] = "_";
+        if ($goalb[$actual - 1][$i1] == "-1") $goalb[$actual - 1][$i1] = "_";
+    }
+    $endtab = $actual;
+    include_once(PATH_TO_LMO . "/lmo-calctable.php");
+
+    for($i1 = 0; $i1 < $anzsp; $i1++) {
+        if ($goala[$actual - 1][$i1] == "_") $goala[$actual - 1][$i1] = "-1";
+        if ($goalb[$actual - 1][$i1] == "_") $goalb[$actual - 1][$i1] = "-1";
+    }
+}
+if ($lmtype==0) {
+    isset($tab0) ? $table1=$tab0 : $table1=$tab1;
+    if (isset($table1)) {
+        $wmlfile= fopen(PATH_TO_LMO.'/'.$diroutput.basename($file)."-sp.html","wb");
+        ob_start();
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
         "http://www.w3.org/TR/html4/loose.dtd">
@@ -60,34 +68,38 @@ if($lmtype==0){
     <tr>
       <td>
 <?php
-    for ($y1=1;$y1<$anzst+1;$y1++) {
-      $datumanz=$y1-1;
-      $z=array_filter($teama[$y1-1],"filterZero");
-      if (!empty($z)) {
+        for ($y1 = 1; $y1 < $anzst + 1; $y1++) {
+            $datumanz = $y1 - 1;
+            $z = array_filter($teama[$y1 - 1], "filterZero");
+            if (!empty($z)) {
 ?>
         <table>
         <caption><?php echo $y1?>.  <?php echo $text[2]?><?php if ($datum1[$datumanz]!='') { echo ' - '.$datum1[$datumanz].' '.$text[4].' '.$datum2[$datumanz];}?></caption>
 <?php
-        $datsort= $mterm[$y1-1];
-        asort($datsort);
-        reset($datsort);
-        //while (list ($key, $val) = each ($datsort)) { //Deprecated: The each() function ...
-        foreach($datsort as $key => $val) {
-          $i1=$key;
-          if(($teama[$y1-1][$i1]>0) && ($teamb[$y1-1][$i1]>0)){
-            $heimteam=$teams[$teama[$y1-1][$i1]];
-            $gastteam=$teams[$teamb[$y1-1][$i1]];
-            $heimtore=applyFactor($goala[$y1-1][$i1],$goalfaktor);
-            $gasttore=applyFactor($goalb[$y1-1][$i1],$goalfaktor);
-            if ($gasttore<0) $gasttore="_";
-            if ($heimtore<0) $heimtore="_";
-            // * Spielfrei-Hack-Beginn1	- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-            if (($anzteams-($anzst/2+1))!=0){
-              $spielfreiaa[$i1]=$teama[$y1-1][$i1];
-              $spielfreibb[$i1]=$teamb[$y1-1][$i1];
-            }
-            // * Spielfrei-Hack-Ende1- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-            if($mterm[$y1-1][$i1]>0){$dum1=date($datf, $mterm[$y1-1][$i1]);}else{$dum1="";} // Anstosszeit einblenden
+                $datsort = $mterm[$y1 - 1];
+                asort($datsort);
+                reset($datsort);
+                //while (list ($key, $val) = each ($datsort)) { //Deprecated: The each() function ...
+                foreach($datsort as $key => $val) {
+                    $i1 = $key;
+                    if (($teama[$y1 - 1][$i1] > 0) && ($teamb[$y1 - 1][$i1] > 0)) {
+                        $heimteam = $teams[$teama[$y1 - 1][$i1]];
+                        $gastteam = $teams[$teamb[$y1 - 1][$i1]];
+                        $heimtore = applyFactor($goala[$y1 - 1][$i1], $goalfaktor);
+                        $gasttore = applyFactor($goalb[$y1 - 1][$i1], $goalfaktor);
+                        if ($gasttore < 0) $gasttore = "_";
+                        if ($heimtore < 0) $heimtore = "_";
+                        //  * Spielfrei-Hack-Beginn1 - Autor: Bernd Hoyer - eMail: info@salzland-info.de
+                        if (($anzteams - ($anzst / 2 + 1)) != 0) {
+                            $spielfreiaa[$i1] = $teama[$y1 - 1][$i1];
+                            $spielfreibb[$i1] = $teamb[$y1 - 1][$i1];
+                        }
+                        //  * Spielfrei-Hack-Ende1- Autor: Bernd Hoyer - eMail: info@salzland-info.de
+                        if ($mterm[$y1-1][$i1] > 0) {
+                            $dum1 = strtr(date($datf, $mterm[$y1-1][$i1]), $trans_lang);
+                        } else {
+                            $dum1 = "";
+                        } // Anstosszeit einblenden
 ?>
           <tr>
             <td><?php echo $dum1?>&nbsp;</td>
@@ -100,47 +112,47 @@ if($lmtype==0){
             <td>:</td>
             <td align="left"><?php echo $gasttore?></td>
 <?php
-            if ($msieg[$y1-1][$i1]==3){
+                        if ($msieg[$y1 - 1][$i1] == 3) {
 ?>
             <td width="2">/</td>
             <td align="right"><?php echo $gasttore?></td>
             <td align="center" width="8">:</td>
             <td align="left"><?php echo $heimtore?></td>
 <?php
-            }
+                        }
 ?>
           </tr>
 <?php
-          }
-        }
-        $actual=$actual+1;
-      }
-      if (!empty($z)) {
+                    }   
+                }
+            $actual=$actual+1;
+            }
+            if (!empty($z)) {
 ?>
         </table>
 <?php
-      }
-      if (($anzteams-($anzst/2+1))!=0){
-        $spielfreicc=array_merge($spielfreiaa,$spielfreibb);
-        unset($spielfreiaa);
-        unset($spielfreibb);
-        $i=1;
-        for ($j=1;$j<$anzteams+1;$j++) {
-          if (!in_array($j,$spielfreicc)) {
-            if ($i==1) {?>
-      <p><small><?php echo $text[4004]?>: <?php
             }
-            echo $teams[$j]?>&nbsp;&nbsp;<?php
-            $i++;
-          }
-        }
+            if (($anzteams - ($anzst / 2 + 1)) != 0) {
+                $spielfreicc = array_merge($spielfreiaa, $spielfreibb);
+                unset($spielfreiaa);
+                unset($spielfreibb);
+                $i = 1;
+                for ($j = 1; $j < $anzteams + 1; $j++) {
+                    if (!in_array($j, $spielfreicc)) {
+                        if ($i == 1) {?>
+      <p><small><?php echo $text[4004]?>: <?php
+                        }
+                        echo $teams[$j]?>&nbsp;&nbsp;<?php
+                        $i++;
+                    }
+                }
 ?>
       </small></p>
 <?php
-        unset($spielfreicc);
-      }
-    }
-    $datumanz=$actual-1;
+                unset($spielfreicc);
+            }
+        }
+        $datumanz = $actual - 1;
 ?>
       </td>
     </tr>
@@ -158,17 +170,17 @@ if($lmtype==0){
       <th align="right" >&nbsp;&nbsp;<?php echo $text[39]?></th>
     </tr>
 <?php
-    for ($i1=0;$i1<$anzteams;$i1++){
-      $platz=$i1+1;
-      $i4=(int)substr($table1[$i1],35,7);
-      $teamname=$teams[$i4];
-      $pluspunkte=applyFactor($punkte[$i4],$pointsfaktor);
-      $minuspunkte=applyFactor($negativ[$i4],$pointsfaktor);
-      $kegelnholz=applyFactor($dtore[$i4],$goalfaktor);
-      $plustore=applyFactor($etore[$i4],$goalfaktor);
-      $minustore=applyFactor($atore[$i4],$goalfaktor);
-      $torverhaeltnis=applyFactor($dtore[$i4],$goalfaktor);
-      $spieleteam=$spiele[$i4];
+        for ($i1 = 0; $i1 < $anzteams; $i1++) {
+            $platz = $i1 + 1;
+            $i4 = (int)substr($table1[$i1], 35, 7);
+            $teamname = $teams[$i4];
+            $pluspunkte = applyFactor($punkte[$i4], $pointsfaktor);
+            $minuspunkte = applyFactor($negativ[$i4], $pointsfaktor);
+            $kegelnholz = applyFactor($dtore[$i4], $goalfaktor);
+            $plustore = applyFactor($etore[$i4], $goalfaktor);
+            $minustore = applyFactor($atore[$i4], $goalfaktor);
+            $torverhaeltnis = applyFactor($dtore[$i4], $goalfaktor);
+            $spieleteam = $spiele[$i4];
 ?>
     <tr>
       <td align="right"><?php echo $platz?>&nbsp;</td>
@@ -196,7 +208,7 @@ if($lmtype==0){
 </html>
 <?php
   }
-    fwrite($wmlfile,ob_get_contents());
+    fwrite($wmlfile, ob_get_contents());
     ob_end_clean();
     fclose($wmlfile);
 }
