ba2cdd9f65019880544df8730ffa973ba9ea425f
Henshingly <dietmar_kersting@web.de>
2023-08-30 10:21:41 +0200

    Update lmo-tipppad.php

--- lmo/addon/tipp/lmo-tipppad.php
+++ lmo/addon/tipp/lmo-tipppad.php
@@ -47,7 +47,7 @@ if(($action=="tipp") && ($todo=="")){
         $auswertfile=PATH_TO_ADDONDIR."/tipp/".$tipp_dirtipp."auswert/".$dummy[$u].".aus";
         if ($tipp_nurgesamt==0) {?>
         <li>
-          <a href="<?php echo $addw.$dummy[$u].".l98"; ?>"><?php echo $ftest2[$u];?></a><?php if(file_exists($auswertfile)){echo "<br><small>".$text['tipp'][83].": ".date("d.m.Y H:i",filemtime($auswertfile))."</small>";}?>
+          <a href="<?php echo $addw.$dummy[$u].".l98"; ?>"><?php echo $ftest2[$u];?></a><?php if(file_exists($auswertfile)){echo "<br><small>".$text['tipp'][83].": ".date("d.m.Y H:i", filemtime($auswertfile))."</small>";}?>
         </li><?php 
         }
       }
@@ -56,7 +56,7 @@ if(($action=="tipp") && ($todo=="")){
   if($tipp_gesamt==1 && ($u>2 || $tipp_nurgesamt==1 && $u==2)){
     $auswertfile=PATH_TO_ADDONDIR."/tipp/".$tipp_dirtipp."auswert/gesamt.aus";?>
         <li>
-          <a href="<?php echo $addw."&amp;all=1"; ?>"><strong><?php echo $text['tipp'][25];?></strong></a><?php if(file_exists($auswertfile)){echo "<br><small>".$text['tipp'][83].": ".date("d.m.Y H:i",filemtime($auswertfile))."</small>";} ?>
+          <a href="<?php echo $addw."&amp;all=1"; ?>"><strong><?php echo $text['tipp'][25];?></strong></a><?php if(file_exists($auswertfile)){echo "<br><small>".$text['tipp'][83].": ".date("d.m.Y H:i", filemtime($auswertfile))."</small>";} ?>
         </li><?php 
   }
   $auswertfile="";?>
