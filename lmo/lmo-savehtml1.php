00db3e7e728154523499dfba4dee1eaa8aaadc8b
dwblmo <dummwiebrot@gmail.com>
2023-10-03 10:08:32 +0200

    Update lmo-savehtml1.php
    datefmt_format() and remove of blanks

--- lmo/lmo-savehtml1.php
+++ lmo/lmo-savehtml1.php
@@ -66,7 +66,7 @@ if($lmtype==0){
       if (!empty($z)) {
 ?>
         <table>
-        <caption><?php echo $y1?>.  <?php echo $text[2]?><?php if ($datum1[$datumanz]!='') { echo ' - '.$datum1[$datumanz].' '.$text[4].' '.$datum2[$datumanz];}?></caption>
+        <caption><?php echo $y1?>. <?php echo $text[2]?><?php if ($datum1[$datumanz]!='') { echo ' - '.$datum1[$datumanz].' '.$text[4].' '.$datum2[$datumanz];}?></caption>
 <?php
         $datsort= $mterm[$y1-1];
         asort($datsort);
@@ -82,20 +82,20 @@ if($lmtype==0){
             if ($gasttore<0) $gasttore="_";
             if ($heimtore<0) $heimtore="_";
             // * Spielfrei-Hack-Beginn1	- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-            if (($anzteams-($anzst/2+1))!=0){
+           if (($anzteams-($anzst/2+1))!=0){
               $spielfreiaa[$i1]=$teama[$y1-1][$i1];
               $spielfreibb[$i1]=$teamb[$y1-1][$i1];
             }
             // * Spielfrei-Hack-Ende1- Autor: Bernd Hoyer - eMail: info@salzland-info.de
-            if($mterm[$y1-1][$i1]>0){$dum1=date($datf, $mterm[$y1-1][$i1]);}else{$dum1="";} // Anstosszeit einblenden
+            if($mterm[$y1-1][$i1]>0){$dum1=datefmt_format($fmt, $mterm[$y1-1][$i1]);}else{$dum1="";} // Anstosszeit einblenden
 ?>
           <tr>
-            <td><?php echo $dum1?>&nbsp;</td>
+            <td><?php echo $dum1?></td>
             <td align="right"><?php echo $heimteam?></td>
             <td><?php echo HTML_icon($heimteam, 'teams');?></td>
             <td>-</td>
             <td><?php echo HTML_icon($gastteam, 'teams');?></td>
-            <td><?php echo $gastteam?>&nbsp;</td>
+            <td><?php echo $gastteam?></td>
             <td align="right"><?php echo $heimtore?></td>
             <td>:</td>
             <td align="left"><?php echo $gasttore?></td>
@@ -130,7 +130,7 @@ if($lmtype==0){
             if ($i==1) {?>
       <p><small><?php echo $text[4004]?>: <?php
             }
-            echo $teams[$j]?>&nbsp;&nbsp;<?php
+            echo $teams[$j]?><?php
             $i++;
           }
         }
@@ -148,14 +148,14 @@ if($lmtype==0){
   <table id="tabelle">
   <caption><?php echo $text[16]?></caption>
     <tr>
-      <th>&nbsp;</th>
-      <th>&nbsp;</th>
-      <th>&nbsp;</th>
+      <th></th>
+      <th></th>
+      <th></th>
       <th><?php echo $text[33]?></th>
       <th><?php echo $namepkt?></th>
-      <th>&nbsp;</th>
-      <th><?php echo $nametor?>&nbsp;</th>
-      <th align="right" >&nbsp;&nbsp;<?php echo $text[39]?></th>
+      <th></th>
+      <th><?php echo $nametor?></th>
+      <th align="right" ><?php echo $text[39]?></th>
     </tr>
 <?php
     for ($i1=0;$i1<$anzteams;$i1++){
@@ -171,21 +171,21 @@ if($lmtype==0){
       $spieleteam=$spiele[$i4];
 ?>
     <tr>
-      <td align="right"><?php echo $platz?>&nbsp;</td>
-      <td><?php echo HTML_icon($teamname, 'teams');?>&nbsp;</td>
-      <td><?php echo $teamname?>&nbsp;</td>
-      <td align="right"><?php echo $spieleteam?>&nbsp;</td>
+      <td align="right"><?php echo $platz?></td>
+      <td><?php echo HTML_icon($teamname, 'teams');?></td>
+      <td><?php echo $teamname?></td>
+      <td align="right"><?php echo $spieleteam?></td>
       <td align="right"><?php echo $pluspunkte?>
 <?php
       if ($minus==2) {?>:</td>
-      <td align="left"><?php echo $minuspunkte?>&nbsp;<?php
+      <td align="left"><?php echo $minuspunkte?><?php
       } else {?>
       </td>
-      <td align="left">&nbsp;<?php
+      <td align="left"><?php
       }
 ?>
-      <td align="right"><?php echo "$plustore:$minustore"?>&nbsp;</td>
-      <td align="right">&nbsp;&nbsp;<?php echo $torverhaeltnis?></td>
+      <td align="right"><?php echo "$plustore:$minustore"?></td>
+      <td align="right"><?php echo $torverhaeltnis?></td>
     </tr>
 <?php
     }
