818efd319cb164cb71d22965339aa6e2a111ab22
Henshingly <dietmar_kersting@web.de>
2024-12-11 13:44:42 +0100

    Update lmo-showdir.php
    if no dir-descr.txt inside folder

--- lmo/lmo-showdir.php
+++ lmo/lmo-showdir.php
@@ -18,16 +18,11 @@
   */
 
 
-
 ?>
 <table class="lmoMiddle" cellspacing="0" cellpadding="0" border="0">
-   <?php /*<tr>
-    <td align="right">
-<?php include(PATH_TO_LMO."/lmo-ligensortierung.php");?></td>
-  </tr>*/?>
   <tr>
     <td align="left"><?php
-include(PATH_TO_LMO."/lmo-dirlist.php");?>
+include(PATH_TO_LMO . "/lmo-dirlist.php");?>
     </td>
   </tr>
   <tr>
@@ -35,38 +30,34 @@ include(PATH_TO_LMO."/lmo-dirlist.php");?>
   </tr>
   <tr>
     <td align="left"><?php
-if ($archivlink==1) {
-  $subdir=str_replace(array('../','./'),array('',''),$subdir);
-  $dirs = get_dir($dirliga.$subdir);
-  natcasesort($dirs);
-  if (!empty($subdir) && substr($subdir,-1)!='/') $subdir.='/';
+if ($archivlink == 1) {
+    $subdir = str_replace(array('../', './'), array('', ''), $subdir);
+    $dirs = get_dir($dirliga . $subdir);
+    natcasesort($dirs);
+    if (!empty($subdir) && substr($subdir, -1) != '/') $subdir .= '/';
 
-  $output='';
-  foreach($dirs as $dir) {
-    $descr=@implode("",file(PATH_TO_LMO."/".$dirliga.$subdir.$dir."/dir-descr.txt"));
-    $output.=  "<tr><td><a href='".$_SERVER['PHP_SELF']."?subdir=".$subdir.$dir."/'>".$dir."</a></td>";
-    if ($descr!="") {
-      $output.= "<td><small>".htmlentities($descr)."</small></td>";
+    $output = '';
+    foreach($dirs as $dir) {
+        $descr = @file_get_contents(PATH_TO_LMO . '/' . $dirliga . $subdir . $dir . "/dir-descr.txt");
+        $output .=  "      <tr><td><a href='" . $_SERVER['PHP_SELF'] . "?subdir=" . $subdir . $dir . "/'>" . $dir . "</a></td>";
+        if ($descr != "") {
+            $output .= "      <td><small>" . htmlentities($descr) . "</small></td>";
+        }
+        $output .= "</tr>";
     }
-    $output.="</tr>";
-  }
 
-  if ($output!='') {?>
+    if ($output != '') {?>
       <table class='lmoInner' cellspacing="0" width="99%">
         <tr>
           <th style="text-align:center" colspan="2"><?php echo $text[509];?></th>
         </tr>
         <?php echo $output?>
       </table><?php
-  }
-  if (strpos($subdir,'/')!==FALSE) {?>
-      <p><a href="<?php echo $_SERVER['PHP_SELF']?>?subdir=<?php echo dirname($subdir).'/'?>"><?php echo $text[5];?> <?php echo $text[562];?></a></p><?php
-  }
+    }
+    if (strpos($subdir, '/') !== FALSE) {?>
+      <p><a href="<?php echo $_SERVER['PHP_SELF']?>?subdir=<?php echo dirname($subdir) . '/'?>"><?php echo $text[5];?> <?php echo $text[562];?></a></p><?php
+    }
 }?>
     </td>
   </tr>
-  <?php /*<tr>
-    <td align="right">
-<?php include(PATH_TO_LMO."/lmo-ligensortierung.php");?></td>
-  </tr>*/?>
 </table>
