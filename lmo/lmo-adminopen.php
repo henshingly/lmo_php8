86ed6b88e75cadcea0f0dce4ae72f3683448a7b9
Henshingly <dietmar_kersting@web.de>
2025-01-09 20:37:32 +0100

    Update lmo-adminopen.php
    If there is no dir-descr.txt file in the league folder, file_get_contents() throws a FALSE. That's why there's an @ in front of it.
    The rest is just source code customization

--- lmo/lmo-adminopen.php
+++ lmo/lmo-adminopen.php
@@ -18,50 +18,50 @@
   */
 
 
-require_once(PATH_TO_LMO."/lmo-admintest.php");
-$subdir=isset($_REQUEST["subdir"])?$_REQUEST["subdir"]:'';
-if (($action=="admin") && ($todo=="open")){
-  $adda=$_SERVER['PHP_SELF']."?action=admin&amp;todo=";
+require_once(PATH_TO_LMO . "/lmo-admintest.php");
+$subdir = isset($_REQUEST["subdir"]) ? $_REQUEST["subdir"] : '';
+if (($action == "admin") && ($todo == "open")){
+    $adda = $_SERVER['PHP_SELF'] . "?action=admin&amp;todo=";
 ?>
 <table class="lmoMiddle" cellspacing="0" cellpadding="0" border="0">
   <tr>
-    <td align="center"><h1><?php echo $text[294]?></h1></td>
+    <td align="center"><h1><?php echo $text[294];?></h1></td>
   </tr>
   <tr>
-    <td align="center"><?php  require(PATH_TO_LMO."/lmo-dirlist.php"); ?></td>
+    <td align="center"><?php require(PATH_TO_LMO . "/lmo-dirlist.php");?></td>
   </tr>
    <tr>
     <td>&nbsp;</td>
   </tr>
   <tr>
     <td align="left"><?php 
-  $subdir=str_replace(array('../','./'),array('',''),$subdir);
-  $dirs = get_dir($dirliga.$subdir);
-  natcasesort($dirs);
-  if (!empty($subdir) && substr($subdir,-1)!='/') $subdir.='/';
+    $subdir = str_replace(array('../','./'), array('',''), $subdir);
+    $dirs = get_dir($dirliga . $subdir);
+    natcasesort($dirs);
+    if (!empty($subdir) && substr($subdir,-1) != '/') $subdir .= '/';
 
-  $output='';
-  foreach($dirs as $dir) {
-    $descr=file_get_contents(PATH_TO_LMO.'/'.$dirliga.$subdir.$dir."/dir-descr.txt");
-    //$descr=@implode("",file($dirliga.$subdir.$dir."/dir-descr.txt"));
-    $output.=  "<tr><td><a href='".$adda."open&amp;subdir=".$subdir.$dir."/'>".$dir."</a></td>";
-    if ($descr!="") {
-      $output.= "<td><small>".htmlentities($descr)."</small></td>";
+    $output = '';
+    foreach($dirs as $dir) {
+        $descr = @file_get_contents(PATH_TO_LMO . '/' . $dirliga . $subdir . $dir . "/dir-descr.txt");
+        //$descr=@implode("",file($dirliga.$subdir.$dir."/dir-descr.txt"));
+        $output .= "<tr><td><a href='" . $adda . "open&amp;subdir=" . $subdir . $dir . "/'>" . $dir . "</a></td>";
+        if ($descr != "") {
+            $output .= "<td><small>" . htmlentities($descr) . "</small></td>";
+        }
+        $output .= "</tr>";
     }
-    $output.="</tr>";
-  }
 
-  if ($output!='') {?>
+    if ($output != '') {?>
       <table class='lmoInner' cellspacing="0" width="99%">
         <tr>
           <th colspan="2"><?php echo $text[509];?></th>
         </tr>
-        <?php echo $output?>
+        <?php echo $output;?>
       </table><?php 
-  }
-  if (strpos($subdir,'/')!==FALSE) {?>
-      <p><a href="<?php echo $adda?>open&amp;subdir=<?php echo dirname($subdir).'/'?>"><?php echo $text[5];?> <?php echo $text[562];?></a></p><?php 
-  }
+    }
+    if (strpos($subdir, '/') !== FALSE) {?>
+      <p><a href="<?php echo $adda;?>open&amp;subdir=<?php echo dirname($subdir) . '/';?>"><?php echo $text[5];?> <?php echo $text[562];?></a></p><?php 
+    }
 ?>
     </td>
   </tr>
