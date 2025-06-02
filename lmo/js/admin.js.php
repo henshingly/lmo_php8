a97c4368556604a0446cb930b2ddec98cd5f70d4
Henshingly <dietmar_kersting@web.de>
2025-06-02 08:44:46 +0200

    Update admin.js.php
    Removed old attempt at a popup window using Bootstrap and Modal.

--- lmo/js/admin.js.php
+++ lmo/js/admin.js.php
@@ -1,17 +1,17 @@
 <?php include("../init.php");?>
 name="lmo3";
 img0 = new Image();
-img0.src = "<?php echo URL_TO_IMGDIR?>/lmo-admin0.gif";
+img0.src = "<?php echo URL_TO_IMGDIR;?>/lmo-admin0.gif";
 img1 = new Image();
-img1.src = "<?php echo URL_TO_IMGDIR?>/lmo-admin1.gif";
+img1.src = "<?php echo URL_TO_IMGDIR;?>/lmo-admin1.gif";
 img2 = new Image();
-img2.src = "<?php echo URL_TO_IMGDIR?>/lmo-admin2.gif";
+img2.src = "<?php echo URL_TO_IMGDIR;?>/lmo-admin2.gif";
 img3 = new Image();
-img3.src = "<?php echo URL_TO_IMGDIR?>/lmo-admin3.gif";
+img3.src = "<?php echo URL_TO_IMGDIR;?>/lmo-admin3.gif";
 img4 = new Image();
-img4.src = "<?php echo URL_TO_IMGDIR?>/lmo-admin4.gif";
+img4.src = "<?php echo URL_TO_IMGDIR;?>/lmo-admin4.gif";
 img5 = new Image();
-img5.src = "<?php echo URL_TO_IMGDIR?>/lmo-admin5.gif";
+img5.src = "<?php echo URL_TO_IMGDIR;?>/lmo-admin5.gif";
 lmotest=true;
 
 function lmoimg (x,y){
@@ -204,7 +204,7 @@ function ateamlmolink(adresse){
   return confirm("<?php echo $text[335] ?>");
 }
 function opencal(feld,startdat){
-  lmocal="<?php echo URL_TO_LMO?>/lmo-admincal.php?abs=lmoedit&feld="+feld;
+  lmocal="<?php echo URL_TO_LMO;?>/lmo-admincal.php?abs=lmoedit&feld="+feld;
   if (startdat!=""){lmocal=lmocal+"&calshow="+startdat;}
   lmowin = window.open(lmocal,"lmocalpop","width=220,height=200,resizable=yes,dependent=yes");
   lmotest=false;
@@ -219,27 +219,3 @@ function fillAll(elm) {
   }
 
 }
-
-/*
-* This is the plugin
-*/
-(function(a){a.createModal=function(b){defaults={title:"",message:"Your Message Goes Here!",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 420px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-header">';html+='<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>';if (b.title.length>0){html+='<h4 class="modal-title">'+b.title+"</h4>"}html+="</div>";html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if (b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
-
-/*
-* Here is how you use it
-*/
-$(function(){    
-    $('.view-pdf').on('click',function(){
-        var pdf_link = $(this).attr('href');
-        //var iframe = '<div class="iframe-container"><iframe src="'+pdf_link+'"></iframe></div>'
-        //var iframe = '<object data="'+pdf_link+'" type="application/pdf"><embed src="'+pdf_link+'" type="application/pdf"></object>'        
-        var iframe = '<object type="application/pdf" data="'+pdf_link+'" width="100%" height="500">No Support</object>'
-        $.createModal({
-            title:'My Title',
-            message: iframe,
-            closeButton:true,
-            scrollable:false
-        });
-        return false;        
-    });    
-})
