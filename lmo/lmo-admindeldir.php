cd1c936b5c6e4d1d47c8e0594736d5b61ea12574
Henshingly <dietmar_kersting@web.de>
2025-06-05 14:13:45 +0200

    code beautify
    no need to update, just better code reading

--- lmo/lmo-admindeldir.php
+++ lmo/lmo-admindeldir.php
@@ -1,71 +1,78 @@
 <?php
-
 /** Liga Manager Online 4
- *
- * http://lmo.sourceforge.net/
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
- *
- */
+  *
+  * http://lmo.sourceforge.net/
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License as
+  * published by the Free Software Foundation; either version 2 of
+  * the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  * General Public License for more details.
+  *
+  * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
+  *
+  *
+  */
+
 require_once(PATH_TO_LMO . "/lmo-admintest.php");
 $addi = $_SERVER['PHP_SELF'] . "?action=admin&amp;todo=delete&amp;del=1&amp;dfile=";
 if ($ftype != "") {
-  $verz = opendir(substr($dirliga, 0, -1));
-  $dummy = array("");
-  while ($files = readdir($verz)) {
-    if (strtolower(substr($files, -4)) == $ftype) {
-      array_push($dummy, $files);
-    }
-  }
-  closedir($verz);
-  array_shift($dummy);
-  sort($dummy);
-  $i = 0;
-  $j = 0;
-  echo"<ul>";
-  for ($k = 0; $k < count($dummy); $k++) {
-    $files = $dummy[$k];
-    $sekt = "";
-    $t0 = "";
-    $datei = fopen($dirliga . $files, "rb");
-    if ($datei) {
-      while (!feof($datei)) {
-        $zeile = fgets($datei, 1000);
-        $zeile = chop($zeile);
-        $zeile = trim($zeile);
-        if ((substr($zeile, 0, 1) == "[") && (substr($zeile, -1) == "]")) {
-          $sekt = substr($zeile, 1, -1);
-        } elseif ((str_contains($zeile, '=')) && (substr($zeile, 0, 1) != ';') && ($sekt == 'Options')) {
-        //elseif ((strpos($zeile, "=") != false) && (substr($zeile, 0, 1) != ";") && ($sekt == "Options")) {
-          $schl = substr($zeile, 0, strpos($zeile, "="));
-          $wert = substr($zeile, strpos($zeile, "=") + 1);
-          if ($schl == "Name") {
-            $t0 = $wert;
-            break;
-          }
+    $verz = opendir(substr($dirliga, 0, -1));
+    $dummy = array("");
+    while ($files = readdir($verz)) {
+        if (strtolower(substr($files, -4)) == $ftype) {
+            array_push($dummy, $files);
         }
-      }
     }
-    fclose($datei);
-    $i++;
-    if ($t0 == "") {
-      $j++;
-      $t0 = $text[507] . " $j";
+    closedir($verz);
+    array_shift($dummy);
+    sort($dummy);
+    $i = 0;
+    $j = 0;
+?>
+                  <ul>
+<?php
+    for ($k = 0; $k < count($dummy); $k++) {
+        $files = $dummy[$k];
+        $sekt = "";
+        $t0 = "";
+        $datei = fopen($dirliga . $files, "rb");
+        if ($datei) {
+            while (!feof($datei)) {
+                $zeile = fgets($datei, 1000);
+                $zeile = chop($zeile);
+                $zeile = trim($zeile);
+                if ((substr($zeile, 0, 1) == "[") && (substr($zeile, -1) == "]")) {
+                    $sekt = substr($zeile, 1, -1);
+                }
+                elseif ((str_contains($zeile, '=')) && (substr($zeile, 0, 1) != ';') && ($sekt == 'Options')) {
+                //elseif ((strpos($zeile, "=") != false) && (substr($zeile, 0, 1) != ";") && ($sekt == "Options")) {
+                    $schl = substr($zeile, 0, strpos($zeile, "="));
+                    $wert = substr($zeile, strpos($zeile, "=") + 1);
+                    if ($schl == "Name") {
+                        $t0 = $wert;
+                        break;
+                    }
+                }
+            }
+        }
+        fclose($datei);
+        $i++;
+        if ($t0 == "") {
+            $j++;
+            $t0 = $text[507] . " $j";
+        }?>
+                    <li><a href="<?php echo $addi . $dirliga . $files; ?>" onclick="return dellmolink(this.href, <?php echo $t0; ?>"><?php echo $t0; ?></a></li>
+<?php
     }
-    echo "<li><a href='{$addi}{$dirliga}{$files}' onclick='return dellmolink(this.href,\"{$t0}\");'>{$t0}</a></li>";
-  }
-  if ($i == 0) {
-    echo "<li>[" . $text[223] . "]</li>";
-  }
-  echo"</ul>";
+    if ($i == 0) {?>
+                    <li>[<?php echo $text[223]; ?>]</li>
+<?php
+    }?>
+                  <ul>
+<?php
 }
--- lmo/lmo-admindelete.php
+++ lmo/lmo-admindelete.php
@@ -1,51 +1,54 @@
 <?php
 /** Liga Manager Online 4
- *
- * http://lmo.sourceforge.net/
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
- *
- */
-require_once(PATH_TO_LMO . "/lmo-admintest.php");
-if (($action == "admin") && ($todo == "delete") && ($_SESSION['lmouserok'] == 2)) {
-  $adda = $_SERVER['PHP_SELF'] . "?action=admin&amp;todo=";
-  if (!isset($del)) {
-    $del = 0;
-  }
-  if ($del == 1) {
-    if (@unlink(PATH_TO_LMO . "/" . $dfile)) {
-      echo getMessage($dfile . " " . $text[297]);
-    } else {
-      echo getMessage($dfile . " " . $text[298], true);
+  *
+  * http://lmo.sourceforge.net/
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU General Public License as
+  * published by the Free Software Foundation; either version 2 of
+  * the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  * General Public License for more details.
+  *
+  * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
+  *
+  *
+  */
+
+require_once(PATH_TO_LMO . '/lmo-admintest.php');
+if (($action == 'admin') && ($todo == 'delete') && ($_SESSION['lmouserok'] == 2)) {
+    $adda = $_SERVER['PHP_SELF'] . '?action=admin&amp;todo=';
+    if (!isset($del)) {
+        $del = 0;
     }
-  }
-  ?>
-  <table class="lmoMiddle" width="99%" cellspacing="0" cellpadding="0" border="0">
-    <tr>
-      <td align="center"><h1><?php echo $text[295]; ?></h1></td>
-    </tr>
-    <tr>
-      <td align="center">
-        <table class="lmoInner" cellspacing="0" cellpadding="0" border="0">
-          <tr>
-            <td class="nobr" align="left"><?php 
-  $ftype = ".l98";
-  require(PATH_TO_LMO . "/lmo-admindeldir.php");
-  ?>
-              </nobr></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-  </table><?php
+    if ($del == 1) {
+        if (@unlink(PATH_TO_LMO . '/' . $dfile)) {
+            echo getMessage($dfile . ' ' . $text[297]);
+        } else {
+        echo getMessage($dfile . ' ' . $text[298], true);
+        }
+    }
+?>
+      <table class="lmoMiddle" width="99%" cellspacing="0" cellpadding="0" border="0">
+        <tr>
+          <td align="center"><h1><?php echo $text[295]; ?></h1></td>
+        </tr>
+        <tr>
+          <td align="center">
+            <table class="lmoInner" cellspacing="0" cellpadding="0" border="0">
+              <tr>
+                <td class="nobr" align="left">
+<?php
+    $ftype = '.l98';
+    require(PATH_TO_LMO . '/lmo-admindeldir.php');
+?>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table><?php
 }
