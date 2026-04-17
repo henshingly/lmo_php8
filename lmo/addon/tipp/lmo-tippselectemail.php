831f119d9d634be78322f7bd9e7575e9a1424141
Henshingly <henshingly@vest-sport.de>
2026-04-17 21:47:23 +0200

    Update lmo-tippselectemail.php
    PHP Warning:  Undefined array key 4 in /addon/tipp/lmo-tippselectemail.php on line 31
    
    I got this error after changing lmo-tippauth.txt to lmo-tippauth.php and the first line of the PHP file contained "<?php exit(); ?>".

--- lmo/addon/tipp/lmo-tippselectemail.php
+++ lmo/addon/tipp/lmo-tippselectemail.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /** Liga Manager Online 4
   *
   * http://lmo.sourceforge.net/
@@ -7,7 +7,7 @@
   * modify it under the terms of the GNU General Public License as
   * published by the Free Software Foundation; either version 2 of
   * the License, or (at your option) any later version.
-  * 
+  *
   * This program is distributed in the hope that it will be useful,
   * but WITHOUT ANY WARRANTY; without even the implied warranty of
   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
@@ -16,19 +16,39 @@
   * REMOVING OR CHANGING THE COPYRIGHT NOTICES IS NOT ALLOWED!
   *
   */
-  
-  
-require_once(PATH_TO_LMO."/lmo-admintest.php");
-$dumma = array();
-$pswfile = PATH_TO_ADDONDIR."/tipp/".$tipp_tippauthtxt;
 
-$dumma = file($pswfile);
- 
-sort($dumma, SORT_STRING);
-for ($i = 0; $i < count($dumma); $i++) {
-  $dummb = explode("|", $dumma[$i]);
-  echo "<option value=\"".$dummb[0]."\" ";
-  echo ">".$dummb[0]." (".$dummb[3]." - ".$dummb[4].")"."</option>";
+
+require_once(PATH_TO_LMO . '/lmo-admintest.php');
+$dumma = array();
+$pswfile = PATH_TO_ADDONDIR . '/tipp/' . $tipp_tippauthtxt;
+
+$dumma = [];
+
+if (file_exists($pswfile)) {
+    $dumma = file($pswfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 }
 
+sort($dumma, SORT_STRING);
+
+foreach ($dumma as $line) {
+
+    // PHP-Zeile überspringen
+    if (str_starts_with(trim($line), '<?php')) {
+        continue;
+    }
+
+    $dummb = explode('|', $line);
+
+    if (count($dummb) < 2) {
+        continue; // ungültige Zeile
+    }
+
+    $nick = $dummb[0] ?? '';
+    $name = $dummb[3] ?? '';
+    $email = $dummb[4] ?? '';
+
+    echo '<option value="' . $nick . '">';
+    echo $nick . ' (' . $name.' - ' . $email . ')';
+    echo '</option>';
+}
 ?>
