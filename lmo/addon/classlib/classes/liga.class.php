611047b60efad7721b7086295bc5e20e838ae3cd
Henshingly <dietmar_kersting@web.de>
2026-06-03 06:26:17 +0200

    Modify return value to null in liga.class.php
    The method is declared as `&factory()` (returning a reference) but returns `false`. In PHP 8.x, this triggers a deprecation notice; in 8.5, it may result in an error.

--- lmo/addon/classlib/classes/liga.class.php
+++ lmo/addon/classlib/classes/liga.class.php
@@ -121,8 +121,9 @@ class liga {
                 return new liga();
             }
         }
-        else
-            return false;
+        else 
+            $null = null;
+        return $null;
     }
 
     /**
