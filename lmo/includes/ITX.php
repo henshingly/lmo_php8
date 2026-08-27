0448441ad18eaf0aaa6c5a2758ad0944da5a444f
Henshingly <dietmar_kersting@web.de>
2026-08-27 09:57:26 +0200

    Aktualisieren von ITX.php

--- lmo/includes/ITX.php
+++ lmo/includes/ITX.php
@@ -286,8 +286,7 @@ class HTML_Template_ITX extends HTML_Template_IT {
 
         } elseif ( count($parents) > 1 ) {
 
-            reset($parents);
-            while (list($k, $parent) = each($parents)) {
+            foreach ($parents as $k => $parent) {
                 $msg .= "$parent, ";
             }
             $msg = substr($parent, -2);
@@ -376,8 +375,7 @@ class HTML_Template_ITX extends HTML_Template_IT {
             if (is_array($variables = $this->blockvariables[$block])) {
 
                 // search the value in the list of blockvariables
-                reset($variables);
-                while (list($k, $variable) = each($variables)) {
+                foreach ($variables as $k => $variable) {
                     if ($k == $placeholder) {
                         $found = $block;
                         break;
@@ -389,8 +387,7 @@ class HTML_Template_ITX extends HTML_Template_IT {
 
             // search all blocks and return the name of the first block that
             // contains the placeholder
-            reset($this->blockvariables);
-            while (list($blockname, $variables) = each($this->blockvariables)){
+            foreach ($this->blockvariables as $blockname => $variables) {
 
                 if (is_array($variables) && isset($variables[$placeholder])) {
                     $found = $blockname;
@@ -412,8 +409,7 @@ class HTML_Template_ITX extends HTML_Template_IT {
     function performCallback()
     {
 
-        reset($this->functions);
-        while (list($func_id, $function) = each($this->functions)) {
+        foreach ($this->functions as $func_id => $function) {
 
             if (isset($this->callback[$function['name']])) {
 
@@ -655,7 +651,7 @@ class HTML_Template_ITX extends HTML_Template_IT {
 
             while ('' != $head && $args2 = $this->getValue($head, ',')) {
                 $arg2 = trim($args2);
-                $args[] = ('"' == $arg2{0} || "'" == $arg2{0}) ?
+                $args[] = ('"' == $arg2[0] || "'" == $arg2[0]) ?
                                     substr($arg2, 1, -1) : $arg2;
                 if ($arg2 == $head) {
                     break;
@@ -733,8 +729,7 @@ class HTML_Template_ITX extends HTML_Template_IT {
             $variables = array($variables => true);
         }
 
-        reset($this->blockvariables[$block]);
-        while (list($varname, $val) = each($this->blockvariables[$block])) {
+        foreach ($this->blockvariables[$block] as $varname => $val) {
             if (isset($variables[$varname])) {
                 unset($this->blockvariables[$block][$varname]);
             }
@@ -786,12 +781,8 @@ class HTML_Template_ITX extends HTML_Template_IT {
     function findPlaceholderBlocks($variable)
     {
         $parents = array();
-        reset($this->blocklist);
-        while (list($blockname, $content) = each($this->blocklist)) {
-            reset($this->blockvariables[$blockname]);
-            while (
-                list($varname, $val) = each($this->blockvariables[$blockname]))
-            {
+        foreach ($this->blocklist as $blockname => $content) {
+            foreach ($this->blockvariables[$blockname] as $varname => $val) {
                 if ($variable == $varname) {
                     $parents[] = $blockname;
                 }
